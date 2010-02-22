cf09f2644b7913ef5d32f14f0b782df87a2d27a9
Ad van der Veer <advanderveer@gmail.com>
2010-02-22 16:38:17 +0100

    Twig_Extension_Core is registration is hardcoded in to the Environment Extensions array at key 0 while it should be registered at key core as can be seen in the addExtension method

--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -69,7 +69,7 @@ class Twig_Environment
     $this->charset            = isset($options['charset']) ? $options['charset'] : 'UTF-8';
     $this->baseTemplateClass  = isset($options['base_template_class']) ? $options['base_template_class'] : 'Twig_Template';
     $this->autoReload         = isset($options['auto_reload']) ? (bool) $options['auto_reload'] : $this->debug;
-    $this->extensions         = array(new Twig_Extension_Core());
+    $this->extensions         = array('core' => new Twig_Extension_Core());
     $this->runtimeInitialized = false;
     $this->setCache(isset($options['cache']) ? $options['cache'] : null);
   }
