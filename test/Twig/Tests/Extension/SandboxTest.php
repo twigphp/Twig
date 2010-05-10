dc7a48eed1a74244b94260c687a22fb05daa2399
Fabien Potencier <fabien.potencier@gmail.com>
2010-05-10 19:26:49 +0200

    removed \ in the test suite to make it compatible with PHP 5.2

--- test/Twig/Tests/Extension/SandboxTest.php
+++ test/Twig/Tests/Extension/SandboxTest.php
@@ -11,7 +11,7 @@
 
 require_once dirname(__FILE__).'/../bootstrap.php';
 
-class Twig_Tests_Extension_SandboxTest extends \PHPUnit_Framework_TestCase
+class Twig_Tests_Extension_SandboxTest extends PHPUnit_Framework_TestCase
 {
   static protected $params, $templates;
 
