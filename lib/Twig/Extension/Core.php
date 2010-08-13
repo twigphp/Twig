5213b482dd794600135370b02e616af68671d0a0
Fabien Potencier <fabien.potencier@gmail.com>
2010-08-13 08:20:22 +0200

    removed obsolete code

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -29,7 +29,6 @@ class Twig_Extension_Core extends Twig_Extension
             new Twig_TokenParser_Macro(),
             new Twig_TokenParser_Import(),
             new Twig_TokenParser_Set(),
-            new Twig_TokenParser_Debug(),
         );
     }
 
