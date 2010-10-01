b034e6545d2a1a1c1b84d736c41a6531fb65e10f
Fabien Potencier <fabien.potencier@gmail.com>
2010-10-01 20:34:31 +0200

    fixed typo

--- lib/Twig/Node/Expression/ExtensionReference.php
+++ lib/Twig/Node/Expression/ExtensionReference.php
@@ -30,9 +30,6 @@ class Twig_Node_Expression_ExtensionReference extends Twig_Node_Expression
      */
     public function compile($compiler)
     {
-        $compiler
-            ->addDebugInfo($this)
-            ->raw(sprintf("\$this->env->getExtension('%s')", $this['name']))
-        ;
+        $compiler->raw(sprintf("\$this->env->getExtension('%s')", $this['name']));
     }
 }
