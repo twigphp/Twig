5198d20bb18d7ee82bb3a08894f15a0bbd39cbb0
Fabien Potencier <fabien.potencier@gmail.com>
2010-10-01 12:08:57 +0200

    removed obsolete code

--- lib/Twig/TokenParser/Display.php
+++ lib/Twig/TokenParser/Display.php
@@ -21,9 +21,7 @@ class Twig_TokenParser_Display extends Twig_TokenParser
     {
         $lineno = $token->getLine();
         $name = $this->parser->getStream()->expect(Twig_Token::NAME_TYPE)->getValue();
-        if (!$this->parser->hasBlock($name)) {
-            throw new Twig_SyntaxError("The block '$name' cannot be displayed as it has not yet been defined", $lineno);
-        }
+
         $this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
 
         return new Twig_Node_BlockReference($name, $lineno, $this->getTag());
