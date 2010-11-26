58c65ce0951f562ce359538ccc9827ecc764571d
Jordi Boggiano <j.boggiano@seld.be>
2010-11-26 18:58:49 +0100

    Allow is defined test on complex variables

--- lib/Twig/Node/Expression/Test.php
+++ lib/Twig/Node/Expression/Test.php
@@ -24,16 +24,18 @@ class Twig_Node_Expression_Test extends Twig_Node_Expression
 
         // defined is a special case
         if ('defined' === $this->getAttribute('name')) {
-            if (!$this->getNode('node') instanceof Twig_Node_Expression_Name){
+            if ($this->getNode('node') instanceof Twig_Node_Expression_Name) {
+                $compiler
+                    ->raw($testMap[$this->getAttribute('name')]->compile().'(')
+                    ->repr($this->getNode('node')->getAttribute('name'))
+                    ->raw(', $context)')
+                ;
+            } elseif ($this->getNode('node') instanceof Twig_Node_Expression_GetAttr) {
+                $this->getNode('node')->setAttribute('is_defined_test', true);
+                $compiler->subcompile($this->getNode('node'));
+            } else {
                 throw new Twig_Error_Syntax('The "defined" test only works with simple variables', $this->getLine());
             }
-
-            $compiler
-                ->raw($testMap[$this->getAttribute('name')]->compile().'(')
-                ->repr($this->getNode('node')->getAttribute('name'))
-                ->raw(', $context)')
-            ;
-
             return;
         }
 
