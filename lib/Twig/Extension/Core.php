9d77cf2254a5bc8b51fb11f60e80cb0ae5a81439
Fabien Potencier <fabien.potencier@gmail.com>
2010-12-19 11:26:38 +0100

    added from tag

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -28,6 +28,7 @@ class Twig_Extension_Core extends Twig_Extension
             new Twig_TokenParser_Filter(),
             new Twig_TokenParser_Macro(),
             new Twig_TokenParser_Import(),
+            new Twig_TokenParser_From(),
             new Twig_TokenParser_Set(),
             new Twig_TokenParser_Spaceless(),
         );
