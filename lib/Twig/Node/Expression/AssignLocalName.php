ad6374434dc39dd2be747cbc0ce6da8e397ffa6a
Arnaud Le Blanc <arnaud.lb@gmail.com>
2010-12-23 22:07:22 +0100

    Added nodes representing local variables
    Allows to use local PHP variables as temporary variables in compiled
    templates

---
+++ lib/Twig/Node/Expression/AssignLocalName.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) 2009 Fabien Potencier
+ * (c) 2010 Arnaud Le Blanc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Represents a local private variable.
+ * 
+ * Such variables are not visible from templates.
+ *
+ * @package    twig
+ * @author     Arnaud Le Blanc <arnaud.lb@gmail.com>
+ */
+class Twig_Node_Expression_AssignLocalName extends Twig_Node_Expression_LocalName
+{
+}
