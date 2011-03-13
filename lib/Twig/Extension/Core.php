7d3076c6bdbcbdd16cfa1484fcda14574683bab7
Josiah <josiah@web-dev.com.au>
2011-03-13 18:38:59 +1000

    Added exception handling around the date extension - simply passes through any dates that are not of the correct format.

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -202,7 +202,14 @@ class Twig_Extension_Core extends Twig_Extension
 function twig_date_format_filter($date, $format = 'F j, Y H:i')
 {
     if (!$date instanceof DateTime) {
-        $date = new DateTime((ctype_digit($date) ? '@' : '').$date);
+    	try
+    	{
+        	$date = new DateTime((ctype_digit($date) ? '@' : '').$date);
+		}
+		catch( Exception $exception )
+		{
+			return $date;
+		}
     }
 
     return $date->format($format);
