214af8f9b5588177df458ac736e2203e8c20d413
Jordi Boggiano <j.boggiano@seld.be>
2011-03-23 16:39:25 +0100

    Ensure all mbstring calls have the charset specified

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -423,8 +423,8 @@ if (function_exists('mb_get_info')) {
     function twig_capitalize_string_filter(Twig_Environment $env, $string)
     {
         if (null !== ($charset = $env->getCharset())) {
-            return mb_strtoupper(mb_substr($string, 0, 1, $charset)).
-                         mb_strtolower(mb_substr($string, 1, mb_strlen($string), $charset), $charset);
+            return mb_strtoupper(mb_substr($string, 0, 1, $charset), $charset).
+                         mb_strtolower(mb_substr($string, 1, mb_strlen($string, $charset), $charset), $charset);
         }
 
         return ucfirst(strtolower($string));
