945b0bce9e52efa9c558bb0ae390ac1667692f4d
Fabien Potencier <fabien.potencier@gmail.com>
2011-04-12 09:22:15 +0200

    refactored some tests

--- test/Twig/Tests/FileCachingTest.php
+++ test/Twig/Tests/FileCachingTest.php
@@ -1,26 +1,29 @@
 <?php
 
-class Twig_Tests_FileCachingTest extends PHPUnit_Framework_TestCase
+require_once dirname(__FILE__).'/TestCase.php';
+
+class Twig_Tests_FileCachingTest extends Twig_Tests_TestCase
 {
     protected $fileName;
-    protected $tmpDir;
     protected $env;
 
-    function setUp()
+    public function setUp()
     {
-        $this->tmpDir = sys_get_temp_dir().'/TwigCache';
-        if (!file_exists($this->tmpDir)) {
-            @mkdir($this->tmpDir, 0777, true);;
-        }
-
-        if (!is_writable($this->tmpDir)) {
-            $this->markTestSkipped(sprintf('Cannot write to %s, cannot test file caching.', $this->tmpDir));
-        }
-        $this->env = new Twig_Environment(new Twig_Loader_String(), array('cache' => $this->tmpDir));
         parent::setUp();
+
+        $this->env = new Twig_Environment(new Twig_Loader_String(), array('cache' => $this->tmpDir));
     }
 
-    function testWritingCacheFiles()
+    public function tearDown()
+    {
+        if ($this->fileName) {
+            unlink($this->fileName);
+        }
+
+        parent::tearDown();
+    }
+
+    public function testWritingCacheFiles()
     {
         $name = 'This is just text.';
         $template = $this->env->loadTemplate($name);
@@ -30,7 +33,7 @@ class Twig_Tests_FileCachingTest extends PHPUnit_Framework_TestCase
         $this->fileName = $cacheFileName;
     }
 
-    function testClearingCacheFiles()
+    public function testClearingCacheFiles()
     {
         $name = 'I will be deleted.';
         $template = $this->env->loadTemplate($name);
@@ -39,32 +42,5 @@ class Twig_Tests_FileCachingTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(file_exists($cacheFileName), 'Cache file does not exist.');
         $this->env->clearCacheFiles();
         $this->assertFalse(file_exists($cacheFileName), 'Cache file was not cleared.');
-    }
-
-    function tearDown()
-    {
-        if ($this->fileName) {
-            unlink($this->fileName);
-        }
-        $this->removeDir($this->tmpDir);
-        parent::tearDown();
-    }
-
-    private function removeDir($target)
-    {
-        $fp = opendir($target);
-        while (false !== $file = readdir($fp)) {
-            if (in_array($file, array('.', '..'))) {
-                continue;
-            }
-
-            if (is_dir($target.'/'.$file)) {
-                self::removeDir($target.'/'.$file);
-            } else {
-                unlink($target.'/'.$file);
-            }
-        }
-        closedir($fp);
-        rmdir($target);
     }
 }
