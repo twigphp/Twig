twigphp/Twig

commit 1e09c7b660856b8178c120328b7a13a371f109e4

added template file information when an error occurs during compilation

lib/Twig/Environment.php
@@ -298,11 +298,13 @@ class Twig_Environment
         }
 
         if (!class_exists($cls, false)) {
+            $code = $this->compileSource($this->loader->getSource($name), $name);
+
             if (false === $cache = $this->getCacheFilename($name)) {
-                eval('?>'.$this->compileSource($this->loader->getSource($name), $name));
+                eval('?>'.$code);
             } else {
                 if (!file_exists($cache) || ($this->isAutoReload() && !$this->loader->isFresh($name, filemtime($cache)))) {
-                    $this->writeCacheFile($cache, $this->compileSource($this->loader->getSource($name), $name));
+                    $this->writeCacheFile($cache, $code);
                 }
 
                 require_once $cache;
@@ -459,7 +461,14 @@ class Twig_Environment
      */
     public function compileSource($source, $name = null)
     {
-        return $this->compile($this->parse($this->tokenize($source, $name)));
+        try {
+            return $this->compile($this->parse($this->tokenize($source, $name)));
+        } catch (Twig_Error $e) {
+            $e->setTemplateFile($name);
+            throw $e;
+        } catch (Exception $e) {
+            throw new Twig_Error_Runtime(sprintf('An exception has been thrown during the compilation of a template ("%s").', $e->getMessage()), -1, $name, $e);
+        }
     }
 
     /**

commit 04fcc0ca76d92e98bd971bbde58e3e0540b83915

fixes performance regression
The regression was introduced by 1e09c7b660856b8178c1.

lib/Twig/Environment.php
@@ -298,13 +298,11 @@ class Twig_Environment
         }
 
         if (!class_exists($cls, false)) {
-            $code = $this->compileSource($this->loader->getSource($name), $name);
-
             if (false === $cache = $this->getCacheFilename($name)) {
-                eval('?>'.$code);
+                eval('?>'.$this->compileSource($this->loader->getSource($name), $name));
             } else {
                 if (!file_exists($cache) || ($this->isAutoReload() && !$this->loader->isFresh($name, filemtime($cache)))) {
-                    $this->writeCacheFile($cache, $code);
+                    $this->writeCacheFile($cache, $this->compileSource($this->loader->getSource($name), $name));
                 }
 
                 require_once $cache;