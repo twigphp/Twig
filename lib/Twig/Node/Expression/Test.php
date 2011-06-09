e7604832875a3bf42b57e396ff653b9ac7b774c2
nikic <+@ni-po.com>
2011-06-09 20:50:48 +0200

    Make `a.b is defined` not throw an exception if a is not defined (in strict mode)

--- lib/Twig/Node/Expression/Test.php
+++ lib/Twig/Node/Expression/Test.php
@@ -22,19 +22,15 @@ class Twig_Node_Expression_Test extends Twig_Node_Expression
             throw new Twig_Error_Syntax(sprintf('The test "%s" does not exist', $this->getAttribute('name')), $this->getLine());
         }
 
+        $name = $this->getAttribute('name');
+        $node = $this->getNode('node');
+
         // defined is a special case
-        if ('defined' === $this->getAttribute('name')) {
-            if ($this->getNode('node') instanceof Twig_Node_Expression_Name) {
-                $compiler
-                    ->raw($testMap['defined']->compile().'(')
-                    ->repr($this->getNode('node')->getAttribute('name'))
-                    ->raw(', $context)')
-                ;
-            } elseif ($this->getNode('node') instanceof Twig_Node_Expression_GetAttr) {
-                $this->getNode('node')->setAttribute('is_defined_test', true);
-                $compiler
-                    ->subcompile($this->getNode('node'))
-                ;
+        if ('defined' === $name) {
+            if ($node instanceof Twig_Node_Expression_Name || $node instanceof Twig_Node_Expression_GetAttr) {
+                $node->setAttribute('is_defined_test', true);
+                $compiler->subcompile($node);
+                $node->removeAttribute('is_defined_test');
             } else {
                 throw new Twig_Error_Syntax('The "defined" test only works with simple variables', $this->getLine());
             }
@@ -42,16 +38,16 @@ class Twig_Node_Expression_Test extends Twig_Node_Expression
         }
 
         $compiler
-            ->raw($testMap[$this->getAttribute('name')]->compile().'(')
-            ->subcompile($this->getNode('node'))
+            ->raw($testMap[$name]->compile().'(')
+            ->subcompile($node)
         ;
 
         if (null !== $this->getNode('arguments')) {
             $compiler->raw(', ');
 
             $max = count($this->getNode('arguments')) - 1;
-            foreach ($this->getNode('arguments') as $i => $node) {
-                $compiler->subcompile($node);
+            foreach ($this->getNode('arguments') as $i => $arg) {
+                $compiler->subcompile($arg);
 
                 if ($i != $max) {
                     $compiler->raw(', ');
