0b6cefbb6a6a23753fdf2afc7b4711e094c461e5
Derick Rethans <github@derickrethans.nl>
2011-07-07 15:43:13 +0100

    Fixed TWIG_PROPERTY to return a NULL instead of a zval(NULL).

--- twig.c
+++ twig.c
@@ -328,9 +328,14 @@ zval *TWIG_PROPERTY(zval *object, zval *propname)
 	}
 
 	if (Z_OBJ_HT_P(object)->read_property) {
-		tmp = Z_OBJ_HT_P(object)->read_property(object, propname, BP_VAR_R TSRMLS_CC);
-		return tmp;
+		tmp = Z_OBJ_HT_P(object)->read_property(object, propname, BP_VAR_IS TSRMLS_CC);
+		if (&tmp != &EG(uninitialized_zval_ptr)) {
+			return tmp;
+		} else {
+			return NULL;
+		}
 	}
+	return tmp;
 }
 
 zval *TWIG_PROPERTY_CHAR(zval *object, char *propname)
