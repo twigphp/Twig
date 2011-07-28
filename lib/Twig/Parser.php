ef485402e04a419cc2f6f52b5d795d750b016fa4
Fabien Potencier <fabien.potencier@gmail.com>
2011-07-28 14:32:07 +0200

    fixed Parser bug

--- lib/Twig/Parser.php
+++ lib/Twig/Parser.php
@@ -310,7 +310,7 @@ class Twig_Parser implements Twig_ParserInterface
         }
 
         foreach ($node as $k => $n) {
-            if (null === $n = $this->filterBodyNodes($n)) {
+            if ($n instanceof Twig_NodeInterface && null === $n = $this->filterBodyNodes($n)) {
                 $node->removeNode($k);
             }
         }
