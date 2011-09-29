a188f970401dfc7db7517ebba08668ee33e549a1
Fabien Potencier <fabien.potencier@gmail.com>
2011-09-29 16:44:28 +0200

    fixed unit test broken by previous merge

--- test/Twig/Tests/Node/Expression/Binary/FloorDivTest.php
+++ test/Twig/Tests/Node/Expression/Binary/FloorDivTest.php
@@ -43,7 +43,7 @@ class Twig_Tests_Node_Expression_Binary_FloorDivTest extends Twig_Tests_Node_Tes
         $node = new Twig_Node_Expression_Binary_FloorDiv($left, $right, 0);
 
         return array(
-            array($node, 'floor((1 / 2))'),
+            array($node, 'intval(floor((1 / 2)))'),
         );
     }
 }
