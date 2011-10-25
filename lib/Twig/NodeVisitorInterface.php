c48bd2f8099e55c8e0115bed8508827a9b73ea51
Nils Langner <nils.langner@phmlabs.com>
2011-10-25 14:32:16 +0200

    Fixed wrong used PHPDoc tags

--- lib/Twig/NodeVisitorInterface.php
+++ lib/Twig/NodeVisitorInterface.php
@@ -23,7 +23,7 @@ interface Twig_NodeVisitorInterface
      * @param Twig_NodeInterface $node The node to visit
      * @param Twig_Environment   $env  The Twig environment instance
      *
-     * @param Twig_NodeInterface The modified node
+     * @return Twig_NodeInterface The modified node
      */
     function enterNode(Twig_NodeInterface $node, Twig_Environment $env);
 
@@ -33,7 +33,7 @@ interface Twig_NodeVisitorInterface
      * @param Twig_NodeInterface $node The node to visit
      * @param Twig_Environment   $env  The Twig environment instance
      *
-     * @param Twig_NodeInterface The modified node
+     * @return Twig_NodeInterface The modified node
      */
     function leaveNode(Twig_NodeInterface $node, Twig_Environment $env);
 
