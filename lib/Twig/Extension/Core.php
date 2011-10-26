3201b39f47b08854d883213ff3323646086cf213
Fabien Potencier <fabien.potencier@gmail.com>
2011-10-26 11:28:33 +0200

    added missing supported charset for htmlspecialchars

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -516,14 +516,15 @@ function twig_escape_filter(Twig_Environment $env, $string, $type = 'html', $cha
                 'iso-8859-15', 'iso8859-15',
                 'utf-8',
                 'cp866', 'ibm866', '866',
-                'cp1251', 'win-1251', '1251',
-                'cp1252', '1252',
+                'cp1251', 'windows-1251', 'win-1251', '1251',
+                'cp1252', 'windows-1252', '1252',
                 'koi8-r', 'koi8-ru', 'koi8r',
                 'big5', '950',
                 'gb2312', '936',
-                'big5-hkscs', 'big5',
+                'big5-hkscs',
                 'shift_jis', 'sjis', '932',
                 'euc-jp', 'eucjp',
+                'iso8859-5', 'iso-8859-5', 'macroman',
             ))) {
                 return htmlspecialchars($string, ENT_QUOTES | ENT_SUBSTITUTE, $charset);
             }
