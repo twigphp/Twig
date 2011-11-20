f7f8346d37d41524834257b180f41fe86a096b91
Derick Rethans <github@derickrethans.nl>
2011-11-20 21:26:21 +0000

    Switch LICENSE to Revised BSD.

--- ext/twig/twig.c
+++ ext/twig/twig.c
@@ -4,13 +4,9 @@
    +----------------------------------------------------------------------+
    | Copyright (c) 2011 Derick Rethans                                    |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 2.02 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available at through the world-wide-web at                           |
-   | http://www.php.net/license/2_02.txt.                                 |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
+   | Redistribution and use in source and binary forms, with or without   |
+   | modification, are permitted provided that the conditions mentioned   |
+   | in the accompanying LICENSE file are met (BSD, revised).             |
    +----------------------------------------------------------------------+
    | Author: Derick Rethans <derick@derickrethans.nl>                     |
    +----------------------------------------------------------------------+
