d7f57df82a3fb5ce5535d5e04594b7088a8fa966
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2011-12-05 12:43:41 +1100

    Removed trailing white spaces

--- lib/Twig/Node/Expression/Function.php
+++ lib/Twig/Node/Expression/Function.php
@@ -18,24 +18,24 @@ class Twig_Node_Expression_Function extends Twig_Node_Expression
     public function compile(Twig_Compiler $compiler)
     {
         $name = $this->getAttribute('name');
-        
+
         $function = $compiler->getEnvironment()->getFunction($name);
-        
+
         if (false === $function) {
             $alternativeFunctions = array();
-            
+
             foreach ($compiler->getEnvironment()->getFunctions() as $functionName => $function) {
                 if (false !== strpos($functionName, $name)) {
                     $alternativeFunctions[] = $functionName;
                 }
             }
-            
+
             $exceptionMessage = sprintf('The function "%s" does not exist', $name);
-           
+
             if (count($alternativeFunctions)) {
                 $exceptionMessage = sprintf('%s. Did you mean "%s"?', $exceptionMessage, implode('", "', $alternativeFunctions));
             }
-            
+
             throw new Twig_Error_Syntax($exceptionMessage, $this->getLine());
         }
 
