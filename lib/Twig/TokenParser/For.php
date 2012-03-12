4bd63f64851a34f6e9b4f4082af5e9f083c51822
badlee <badinga.ulrich@gmail.com>
2012-03-12 18:33:25 +0100

    Add Twig_TokenParser_break in lib/Twig/TokenParser/For.php

--- lib/Twig/TokenParser/For.php
+++ lib/Twig/TokenParser/For.php
@@ -6,6 +6,8 @@
  * (c) 2009 Fabien Potencier
  * (c) 2009 Armin Ronacher
  *
+ * Updater Badlee Oshimin 2012 - add break tag
+ * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -87,3 +89,47 @@ class Twig_TokenParser_For extends Twig_TokenParser
         return 'for';
     }
 }
+
+
+/**
+ * breaking a Loop.
+ *
+ * <pre>
+ * <i>Show letter a to n</i>
+ * <ul>
+ *  {% for i in 'a'..'z' %}
+ *    <li>{{ i }}</li>
+ *    {% if i == 'n'%}
+ *      {% break %}
+ *    {% endif %}
+ *  {% endfor %}
+ * </ul>
+ * </pre>
+ */
+class Twig_TokenParser_break extends Twig_TokenParser
+{
+    /**
+     * Gets the tag name associated with this token parser.
+     *
+     * @return string The tag name
+     */
+    public function getTag()
+    {
+        return $this->'break';
+    }
+    
+    /**
+     * Parses a token and returns a node.
+     *
+     * @param Twig_Token $token A Twig_Token instance
+     *
+     * @return Twig_NodeInterface A Twig_NodeInterface instance
+     */
+    public function parse(Twig_Token $token)
+    {
+    	$lineno = $token->getLine();
+    	$this->parser->getStream()->expect(Twig_Token::BLOCK_END_TYPE);
+        return new Twig_Node_break($lineno,  $this->getTag());
+    }
+}
+
