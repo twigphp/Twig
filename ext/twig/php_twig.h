e2220bb2826dd3eb6720fbdf5f175fb013f9b437
Fabien Potencier <fabien.potencier@gmail.com>
2012-04-03 09:56:57 +0200

    bumped version to 1.7.0-DEV

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.6.5-DEV"
+#define PHP_TWIG_VERSION "1.7.0-DEV"
 
 #include "php.h"
 
--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -17,7 +17,7 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.6.5-DEV';
+    const VERSION = '1.7.0-DEV';
 
     protected $charset;
     protected $loader;
