twigphp/Twig

commit aa6b8358161e4ef35299be06a5ff61bfcba46f6a

converted item parameter of Twig_Template::getAttribute() to string

lib/Twig/Template.php
@@ -308,6 +308,8 @@ abstract class Twig_Template implements Twig_TemplateInterface
      */
     protected function getAttribute($object, $item, array $arguments = array(), $type = Twig_TemplateInterface::ANY_CALL, $isDefinedTest = false, $ignoreStrictCheck = false)
     {
+        $item = (string) $item;
+
         // array
         if (Twig_TemplateInterface::METHOD_CALL !== $type) {
             if ((is_array($object) && array_key_exists($item, $object))

test/Twig/Tests/TemplateTest.php
@@ -68,6 +68,7 @@ class Twig_Tests_TemplateTest extends PHPUnit_Framework_TestCase
             'defined' => 'defined',
             'zero'    => 0,
             'null'    => null,
+            '1'       => 1,
         );
 
         $objectArray         = new Twig_TemplateArrayAccessObject;
@@ -85,6 +86,8 @@ class Twig_Tests_TemplateTest extends PHPUnit_Framework_TestCase
             array(true,  'defined', 'defined'),
             array(false, null,      'undefined'),
             array(true,  0,         'zero'),
+            array(true,  1,         1),
+            array(true,  1,         1.0),
             array(true,  null,      'null'),
         );
         $testObjects = array(
@@ -99,6 +102,11 @@ class Twig_Tests_TemplateTest extends PHPUnit_Framework_TestCase
         $tests = array();
         foreach ($testObjects as $testObject) {
             foreach ($basicTests as $test) {
+                // properties cannot be numbers
+                if ($testObject[0] instanceof stdClass && is_numeric($test[2])) {
+                    continue;
+                }
+
                 $tests[] = array($test[0], $test[1], $testObject[0], $test[2], array(), $testObject[1]);
             }
         }
@@ -154,6 +162,7 @@ class Twig_TemplateArrayAccessObject implements ArrayAccess
         'defined' => 'defined',
         'zero'    => 0,
         'null'    => null,
+        '1'       => 1,
     );
 
     public function offsetExists($name)
@@ -181,6 +190,7 @@ class Twig_TemplateMagicPropertyObject
         'defined' => 'defined',
         'zero'    => 0,
         'null'    => null,
+        '1'       => 1,
     );
 
     public function __isset($name)
@@ -199,6 +209,11 @@ class Twig_TemplateMethodObject
     public function getDefined()
     {
         return 'defined';
+    }
+
+    public function get1()
+    {
+        return 1;
     }
 
     public function getZero()

commit 8ec73cf4759f90b961de1732da1732e60c1dbfff

fixed a regression introduced by aa6b8358161e4ef35299be06a5ff61bfcba46f6a

ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.9.0-DEV"
+#define PHP_TWIG_VERSION "1.8.1-DEV"
 
 #include "php.h"
 

lib/Twig/Environment.php
@@ -17,7 +17,7 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.9.0-DEV';
+    const VERSION = '1.8.1-DEV';
 
     protected $charset;
     protected $loader;

lib/Twig/Template.php
@@ -326,7 +326,7 @@ abstract class Twig_Template implements Twig_TemplateInterface
      */
     protected function getAttribute($object, $item, array $arguments = array(), $type = Twig_TemplateInterface::ANY_CALL, $isDefinedTest = false, $ignoreStrictCheck = false)
     {
-        $item = (string) $item;
+        $item = is_numeric($item) ? (int) $item : (string) $item;
 
         // array
         if (Twig_TemplateInterface::METHOD_CALL !== $type) {

test/Twig/Tests/Fixtures/regression/simple_xml_element.test
@@ -1,0 +1,15 @@
+--TEST--
+Twig is able to deal with SimpleXMLElement instances as variables
+--TEMPLATE--
+Hello '{{ images.image.0.group }}'!
+{{ images.children().count() }}
+{% for image in images %}
+    - {{ image.group }}
+{% endfor %}
+--DATA--
+return array('images' => new SimpleXMLElement('<images><image><group>foo</group></image><image><group>bar</group></image></images>'))
+--EXPECT--
+Hello 'foo'!
+2
+    - foo
+    - bar