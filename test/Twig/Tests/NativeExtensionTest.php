db3cb803d2b5c760c943836c27164648bb5f0463
Charles <char101@ui.ac.id>
2012-09-20 18:35:44 +0700

    Fix NativeExtensionTest

--- test/Twig/Tests/NativeExtensionTest.php
+++ test/Twig/Tests/NativeExtensionTest.php
@@ -13,13 +13,16 @@ class Twig_Tests_NativeExtensionTest extends PHPUnit_Framework_TestCase
 {
     public function testGetProperties()
     {
-        $loader = new Twig_Loader_String('{{ d1.date }}{{ d2.date }}');
-
-        $twig = new Twig_Environment($loader, array(
+        $twig = new Twig_Environment(new Twig_Loader_String(), array(
             'debug'      => true,
             'cache'      => false,
-            'autoescape' => array($this, 'escapingStrategyCallback'),
+            'autoescape' => false
         ));
+
+		$twig->render('{{ d1.date }}{{ d2.date }}', array(
+			'd1' => new DateTime,
+			'd2' => new DateTime
+		));
 
 		// If it fails, PHP will crash.
     }
