677d34c55b4033df03679232bb324b4099d738e0
Fabien Potencier <fabien.potencier@gmail.com>
2012-09-28 23:50:11 +0200

    fixed CS

--- test/Twig/Tests/NativeExtensionTest.php
+++ test/Twig/Tests/NativeExtensionTest.php
@@ -11,19 +11,19 @@
 
 class Twig_Tests_NativeExtensionTest extends PHPUnit_Framework_TestCase
 {
-	public function testGetProperties()
-	{
-		$twig = new Twig_Environment(new Twig_Loader_String(), array(
-			'debug'		 => true,
-			'cache'		 => false,
-			'autoescape' => false
-		));
+    public function testGetProperties()
+    {
+        $twig = new Twig_Environment(new Twig_Loader_String(), array(
+            'debug'      => true,
+            'cache'      => false,
+            'autoescape' => false
+        ));
 
-		$d1 = new DateTime;
-		$d2 = new DateTime;
-		$output = $twig->render('{{ d1.date }}{{ d2.date }}', compact('d1', 'd2'));
+        $d1 = new DateTime();
+        $d2 = new DateTime();
+        $output = $twig->render('{{ d1.date }}{{ d2.date }}', compact('d1', 'd2'));
 
-		// If it fails, PHP will crash.
-		$this->assertEquals($output, $d1->date . $d2->date);
-	}
+        // If it fails, PHP will crash.
+        $this->assertEquals($output, $d1->date . $d2->date);
+    }
 }
