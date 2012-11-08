44667ae18ddb1d261d0e877f40f7c51c36e642ac
Charles <char101@ui.ac.id>
2012-11-08 09:16:19 +0700

    Use ternary operator with PHP 5.4 when strict_variables is true

--- lib/Twig/Node/Expression/Name.php
+++ lib/Twig/Node/Expression/Name.php
@@ -44,15 +44,18 @@ class Twig_Node_Expression_Name extends Twig_Node_Expression
             // remove the non-PHP 5.4 version when PHP 5.3 support is dropped
             // as the non-optimized version is just a workaround for slow ternary operator
             // when the context has a lot of variables
-            if (version_compare(phpversion(), '5.4.0RC1', '>=') && ($this->getAttribute('ignore_strict_check') || !$compiler->getEnvironment()->isStrictVariables())) {
+            if (version_compare(phpversion(), '5.4.0RC1', '>=')) {
                 // PHP 5.4 ternary operator performance was optimized
                 $compiler
                     ->raw('(isset($context[')
                     ->string($name)
                     ->raw(']) ? $context[')
-                    ->string($name)
-                    ->raw('] : null)')
-                ;
+                    ->string($name);
+				if ($this->getAttribute('ignore_strict_check') || !$compiler->getEnvironment()->isStrictVariables()) {
+                    $compiler->raw('] : null)');
+				} else {
+                    $compiler->raw('] : $this->getContext($context, ')->string($name)->raw('))');
+				}
             } else {
                 $compiler
                     ->raw('$this->getContext($context, ')
