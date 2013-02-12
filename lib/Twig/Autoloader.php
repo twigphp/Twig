19869ce00ca6eb7b7f0f700776d35bf88f6936f1
Martin Hasoň <martin.hason@gmail.com>
2013-02-12 09:12:17 +0100

    Fixed autoloader registration for tests if phpunit is installed as package together with Twig

--- lib/Twig/Autoloader.php
+++ lib/Twig/Autoloader.php
@@ -18,11 +18,17 @@ class Twig_Autoloader
 {
     /**
      * Registers Twig_Autoloader as an SPL autoloader.
+     *
+     * @param Bolean $prepend Whether to prepend the autoloader or not.
      */
-    public static function register()
+    public static function register($prepend = false)
     {
         ini_set('unserialize_callback_func', 'spl_autoload_call');
-        spl_autoload_register(array(new self, 'autoload'));
+        if ($prepend && version_compare(phpversion(), '5.3.0RC1', '>=')) {
+            spl_autoload_register(array(new self, 'autoload'), true, $prepend);
+        } else {
+            spl_autoload_register(array(new self, 'autoload'), true);
+        }
     }
 
     /**
