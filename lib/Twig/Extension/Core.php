61f2b2a9d81a40de4842c44a307fba7ab07f6840
Jordi Boggiano <j.boggiano@seld.be>
2013-05-06 14:09:40 +0200

    Use plain string comparison which is twice as fast

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -948,7 +948,9 @@ function twig_escape_filter(Twig_Environment $env, $string, $strategy = 'html', 
             return $string;
 
         case 'url':
-            if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+            // hackish test to avoid version_compare that is much slower, this works unless PHP releases a 5.10.*
+            // at that point however PHP 5.2.* support can be removed
+            if (PHP_VERSION < '5.3.0') {
                 return str_replace('%7E', '~', rawurlencode($string));
             }
 
