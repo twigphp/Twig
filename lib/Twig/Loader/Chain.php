8a662f2ab3e47cba3073927d5bf1cedd72dcfb33
Martin Hasoň <martin.hason@gmail.com>
2013-05-15 12:37:17 +0200

    Fixed Twig_Loader_Chain::exists for a loader which implements Twig_ExistsLoaderInterface

--- lib/Twig/Loader/Chain.php
+++ lib/Twig/Loader/Chain.php
@@ -76,8 +76,12 @@ class Twig_Loader_Chain implements Twig_LoaderInterface, Twig_ExistsLoaderInterf
         }
 
         foreach ($this->loaders as $loader) {
-            if ($loader instanceof Twig_ExistsLoaderInterface && $loader->exists($name)) {
-                return $this->hasSourceCache[$name] = true;
+            if ($loader instanceof Twig_ExistsLoaderInterface) {
+                if ($loader->exists($name)) {
+                    return $this->hasSourceCache[$name] = true;
+                }
+
+                continue;
             }
 
             try {
