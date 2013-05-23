693956586776108847a447e86b4e5e92ee0a20be
Fabien Potencier <fabien.potencier@gmail.com>
2013-05-23 18:28:35 +0200

    added support for object instances as the second argument of the constant test (closes #1100)

--- lib/Twig/Node/Expression/Test/Constant.php
+++ lib/Twig/Node/Expression/Test/Constant.php
@@ -28,6 +28,17 @@ class Twig_Node_Expression_Test_Constant extends Twig_Node_Expression_Test
             ->raw('(')
             ->subcompile($this->getNode('node'))
             ->raw(' === constant(')
+        ;
+
+        if ($this->getNode('arguments')->hasNode(1)) {
+            $compiler
+                ->raw('get_class(')
+                ->subcompile($this->getNode('arguments')->getNode(1))
+                ->raw(')."::".')
+            ;
+        }
+
+        $compiler
             ->subcompile($this->getNode('arguments')->getNode(0))
             ->raw('))')
         ;
