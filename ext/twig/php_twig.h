6d6a1009427d1f398c9d40904147bf9f723d5755
Fabien Potencier <fabien.potencier@gmail.com>
2013-08-03 17:35:31 +0200

    prepared the 1.13.2 release

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.13.2-DEV"
+#define PHP_TWIG_VERSION "1.13.2"
 
 #include "php.h"
 
--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,7 +16,7 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.13.2-DEV';
+    const VERSION = '1.13.2';
 
     protected $charset;
     protected $loader;
