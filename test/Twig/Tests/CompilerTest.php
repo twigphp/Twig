3e4c6ab3dc2f100da2ae8ccf0d33e68501e86e11
Fabien Potencier <fabien.potencier@gmail.com>
2013-08-04 08:02:23 +0200

    fixed a bug in some unit tests (closes #1092)

--- test/Twig/Tests/CompilerTest.php
+++ test/Twig/Tests/CompilerTest.php
@@ -21,13 +21,13 @@ class Twig_Tests_CompilerTest extends PHPUnit_Framework_TestCase
         }
 
         $required_locales = array('fr_FR.UTF-8', 'fr_FR.UTF8', 'fr_FR.utf-8', 'fr_FR.utf8', 'French_France.1252');
-        if (false === setlocale(LC_ALL, $required_locales)) {
+        if (false === setlocale(LC_NUMERIC, $required_locales)) {
             $this->markTestSkipped('Could not set any of required locales: ' . implode(", ", $required_locales));
         }
 
         $this->assertEquals('1.2', $compiler->repr(1.2)->getSource());
         $this->assertContains('fr', strtolower(setlocale(LC_NUMERIC, 0)));
 
-        setlocale(LC_ALL, $locale);
+        setlocale(LC_NUMERIC, $locale);
     }
 }
