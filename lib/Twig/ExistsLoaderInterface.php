3da7ff29452165c9cf7c6a06fb4c7aa565f95ec0
Fabien Potencier <fabien.potencier@gmail.com>
2013-08-05 18:31:25 +0200

    emptied deprecated interfaces

--- lib/Twig/ExistsLoaderInterface.php
+++ lib/Twig/ExistsLoaderInterface.php
@@ -18,12 +18,4 @@
  */
 interface Twig_ExistsLoaderInterface
 {
-    /**
-     * Check if we have the source code of a template, given its name.
-     *
-     * @param string $name The name of the template to check if we can load
-     *
-     * @return bool    If the template source code is handled by this loader or not
-     */
-    public function exists($name);
 }
--- lib/Twig/TemplateInterface.php
+++ lib/Twig/TemplateInterface.php
@@ -18,31 +18,4 @@
  */
 interface Twig_TemplateInterface
 {
-    const ANY_CALL    = 'any';
-    const ARRAY_CALL  = 'array';
-    const METHOD_CALL = 'method';
-
-    /**
-     * Renders the template with the given context and returns it as string.
-     *
-     * @param array $context An array of parameters to pass to the template
-     *
-     * @return string The rendered template
-     */
-    public function render(array $context);
-
-    /**
-     * Displays the template with the given context.
-     *
-     * @param array $context An array of parameters to pass to the template
-     * @param array $blocks  An array of blocks to pass to the template
-     */
-    public function display(array $context, array $blocks = array());
-
-    /**
-     * Returns the bound environment for this template.
-     *
-     * @return Twig_Environment The current environment
-     */
-    public function getEnvironment();
 }
