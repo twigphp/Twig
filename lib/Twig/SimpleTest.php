ccde2de56f45ea11c6c7948408de21e1d106dfd4
Fabien Potencier <fabien.potencier@gmail.com>
2013-08-05 18:30:14 +0200

    removed deprecated Twig_Test_* classes

--- lib/Twig/SimpleTest.php
+++ lib/Twig/SimpleTest.php
@@ -14,33 +14,6 @@
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
-class Twig_SimpleTest
+class Twig_SimpleTest extends Twig_Test
 {
-    protected $name;
-    protected $callable;
-    protected $options;
-
-    public function __construct($name, $callable, array $options = array())
-    {
-        $this->name = $name;
-        $this->callable = $callable;
-        $this->options = array_merge(array(
-            'node_class' => 'Twig_Node_Expression_Test',
-        ), $options);
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getCallable()
-    {
-        return $this->callable;
-    }
-
-    public function getNodeClass()
-    {
-        return $this->options['node_class'];
-    }
 }
