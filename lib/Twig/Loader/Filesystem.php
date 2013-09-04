60da74cf1e3d6629ccc2f0ed3cc4082418ffc654
Tobias Schultze <webmaster@tubo-world.de>
2013-09-04 10:39:15 +0200

    remove string casts from loader which was done for template reference objects, but this type of parameter is not contracted by the LoaderInterface

--- lib/Twig/Loader/Filesystem.php
+++ lib/Twig/Loader/Filesystem.php
@@ -199,6 +199,11 @@ class Twig_Loader_Filesystem implements Twig_LoaderInterface, Twig_ExistsLoaderI
         return false;
     }
 
+    protected function normalizeName($name)
+    {
+        return preg_replace('#/{2,}#', '/', strtr($name, '\\', '/'));
+    }
+
     protected function parseName($name, $default = self::MAIN_NAMESPACE)
     {
         if (isset($name[0]) && '@' == $name[0]) {
@@ -213,11 +218,6 @@ class Twig_Loader_Filesystem implements Twig_LoaderInterface, Twig_ExistsLoaderI
         }
 
         return array($default, $name);
-    }
-
-    protected function normalizeName($name)
-    {
-        return preg_replace('#/{2,}#', '/', strtr((string) $name, '\\', '/'));
     }
 
     protected function validateName($name)
