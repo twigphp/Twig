2218bf7829d5ffb1ae408ce67a8367c8b5d44cfb
Luis Cordova <cordoval@gmail.com>
2013-11-11 23:41:54 -0500

    fix typo and docblock for consistency

--- lib/Twig/NodeTraverser.php
+++ lib/Twig/NodeTraverser.php
@@ -12,7 +12,7 @@
 /**
  * Twig_NodeTraverser is a node traverser.
  *
- * It visits all nodes and their children and call the given visitor for each.
+ * It visits all nodes and their children and calls the given visitor for each.
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
@@ -24,8 +24,8 @@ class Twig_NodeTraverser
     /**
      * Constructor.
      *
-     * @param Twig_Environment $env      A Twig_Environment instance
-     * @param array            $visitors An array of Twig_NodeVisitorInterface instances
+     * @param Twig_Environment            $env      A Twig_Environment instance
+     * @param Twig_NodeVisitorInterface[] $visitors An array of Twig_NodeVisitorInterface instances
      */
     public function __construct(Twig_Environment $env, array $visitors = array())
     {
