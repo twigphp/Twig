39eb50c00e2c0b8174dbcd63e0ed45815ff7babf
Fabien Potencier <fabien.potencier@gmail.com>
2013-11-14 09:08:49 +0100

    fixed optimizer max value

--- lib/Twig/NodeVisitor/Optimizer.php
+++ lib/Twig/NodeVisitor/Optimizer.php
@@ -39,7 +39,7 @@ class Twig_NodeVisitor_Optimizer implements Twig_NodeVisitorInterface
      */
     public function __construct($optimizers = -1)
     {
-        if (!is_int($optimizers) || $optimizers > 2) {
+        if (!is_int($optimizers) || $optimizers > (self::OPTIMIZE_FOR | self::OPTIMIZE_RAW_FILTER | self::OPTIMIZE_VAR_ACCESS)) {
             throw new InvalidArgumentException(sprintf('Optimizer mode "%s" is not valid.', $optimizers));
         }
 
