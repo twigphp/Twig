ba1b43d9a9f2996b87883eba6a18d3fe881642ab
Fabien Potencier <fabien.potencier@gmail.com>
2013-12-03 13:58:35 +0100

    added tests for exceptions thrown in __call()

--- lib/Twig/Template.php
+++ lib/Twig/Template.php
@@ -417,6 +417,7 @@ abstract class Twig_Template implements Twig_TemplateInterface
             self::$cache[$class]['methods'] = array_change_key_case(array_flip(get_class_methods($object)));
         }
 
+        $call = false;
         $lcItem = strtolower($item);
         if (isset(self::$cache[$class]['methods'][$lcItem])) {
             $method = (string) $item;
@@ -426,6 +427,7 @@ abstract class Twig_Template implements Twig_TemplateInterface
             $method = 'is'.$item;
         } elseif (isset(self::$cache[$class]['methods']['__call'])) {
             $method = (string) $item;
+            $call = true;
         } else {
             if ($isDefinedTest) {
                 return false;
@@ -451,7 +453,7 @@ abstract class Twig_Template implements Twig_TemplateInterface
         try {
             $ret = call_user_func_array(array($object, $method), $arguments);
         } catch (\BadMethodCallException $e) {
-            if ($ignoreStrictCheck || !$this->env->isStrictVariables()) {
+            if ($call && ($ignoreStrictCheck || !$this->env->isStrictVariables())) {
                 return null;
             }
             throw $e;
