21fd021aaf4c48aec3023b63aeb03d8ef1169d3f
Yosef Deray <yosefderay770@gmail.com>
2014-06-21 23:31:56 -0400

    changed back to require_once

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -5,10 +5,7 @@ if (!defined('ENT_SUBSTITUTE')) {
     define('ENT_SUBSTITUTE', 0);
 }
 
-if (!function_exists('twig_escape')) {
-
-    require dirname(dirname(__FILE__)).'/OutputEscaping/Escaping.php';
-}
+require_once dirname(dirname(__FILE__)).'/OutputEscaping/Escaping.php';
 
 /*
  * This file is part of Twig.
