ce467e60bf923f217246a482bd58cb9ae112dfaf
Xavier Leune <xleune@commentcamarche.net>
2015-01-16 10:32:06 +0100

    FIX #1596 FileSystem::findTemplate now applies realpath on the absolute path before returning it.

--- lib/Twig/Loader/Filesystem.php
+++ lib/Twig/Loader/Filesystem.php
@@ -184,7 +184,7 @@ class Twig_Loader_Filesystem implements Twig_LoaderInterface, Twig_ExistsLoaderI
 
         foreach ($this->paths[$namespace] as $path) {
             if (is_file($path.'/'.$shortname)) {
-                return $this->cache[$name] = $path.'/'.$shortname;
+                return $this->cache[$name] = realpath($path.'/'.$shortname);
             }
         }
 
