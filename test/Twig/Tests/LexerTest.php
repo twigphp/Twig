f875cb162ed0b214f2ed56570a54f03c6e42154b
loostro <loostro@gmail.com>
2015-01-19 12:13:43 +0100

    Update LexerTest.php

--- test/Twig/Tests/LexerTest.php
+++ test/Twig/Tests/LexerTest.php
@@ -15,14 +15,15 @@ class Twig_Tests_LexerTest extends PHPUnit_Framework_TestCase
         $template = "";
         
         $tests = array(
-            "foo \n    {{~ 'bar' }}" => 'foo \nbar',
-            "foo \nbar {{~ 'baz' }}" => 'foo \nbarbaz',
+            "foo \n    {{~ 'bar' }}" => 'foo \n',
+            "foo \nbar {{~ 'baz' }}" => 'foo \nbar',
         );
         $lexer = new Twig_Lexer(new Twig_Environment());
         foreach ($tests as $template => $expected) {
             $stream = $lexer->tokenize($template);
             $stream->expect(Twig_Token::VAR_START_TYPE);
-            $stream->expect(Twig_Token::STRING_TYPE, $expected);
+            $stream->expect(Twig_Token::TEXT_TYPE, $expected);
+            $stream->expect(Twig_Token::BLOCK_START_TYPE);
         }
     }
     
