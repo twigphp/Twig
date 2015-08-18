d4aa0238c358aea7f40403d3d13d0b171f0ff07a
Fabien Potencier <fabien.potencier@gmail.com>
2015-08-18 15:28:19 +0200

    bumped version to 1.21

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.20.1-DEV"
+#define PHP_TWIG_VERSION "1.21.0-DEV"
 
 #include "php.h"
 
--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,7 +16,7 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.20.1-DEV';
+    const VERSION = '1.21.0-DEV';
 
     protected $charset;
     protected $loader;
