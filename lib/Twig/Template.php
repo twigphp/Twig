7b579df77dd91c78c27e7381ab7cfbc8f02f8cfb
Tobias Schultze <webmaster@tubo-world.de>
2015-08-21 16:16:21 +0200

    fix php 5.2 compatibility

--- lib/Twig/Template.php
+++ lib/Twig/Template.php
@@ -156,7 +156,7 @@ abstract class Twig_Template implements Twig_TemplateInterface
         if (null !== $template) {
             // avoid RCEs when sandbox is enabled
             if (!$template instanceof self) {
-                throw new \LogicException('A block must be a method on a Twig_Template instance.');
+                throw new LogicException('A block must be a method on a Twig_Template instance.');
             }
 
             try {
