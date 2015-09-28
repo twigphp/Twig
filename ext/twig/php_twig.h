6f84981ae6c1399e5b1b3d18158b9e24fffe1da8
David Négrier <d.negrier@thecodingmachine.com>
2015-09-28 11:31:24 +0200

    Fix custom escaper null values

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.22.2"
+#define PHP_TWIG_VERSION "1.22.3-DEV"
 
 #include "php.h"
 
--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,7 +16,7 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.22.2';
+    const VERSION = '1.22.3-DEV';
 
     protected $charset;
     protected $loader;
@@ -291,6 +291,12 @@ class Twig_Environment
     /**
      * Gets the template class associated with the given string.
      *
+     * The generated template class is based on the following parameters:
+     *
+     *  * The cache key for the given template;
+     *  * The currently enabled extensions;
+     *  * Whether the Twig C extension is available or not.
+     *
      * @param string $name  The name for which to calculate the template class name
      * @param int    $index The index if it is an embedded template
      *
@@ -299,6 +305,10 @@ class Twig_Environment
     public function getTemplateClass($name, $index = null)
     {
         $key = $this->getLoader()->getCacheKey($name).'__'.implode('__', array_keys($this->extensions));
+
+        if (function_exists('twig_template_get_attributes')) {
+            $key .= '__cext';
+        }
 
         return $this->templateClassPrefix.hash('sha256', $key).(null === $index ? '' : '_'.$index);
     }
