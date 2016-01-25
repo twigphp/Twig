62e8ee31246bee48845116863c7ada3ed0f01ef0
Fabien Potencier <fabien.potencier@gmail.com>
2016-01-25 16:54:14 +0100

    fixed the defined test when used on a constant, a map, or a sequence

--- lib/Twig/Node/Expression/Test/Defined.php
+++ lib/Twig/Node/Expression/Test/Defined.php
@@ -25,17 +25,19 @@ class Twig_Node_Expression_Test_Defined extends Twig_Node_Expression_Test
 {
     public function __construct(Twig_NodeInterface $node, $name, Twig_NodeInterface $arguments = null, $lineno)
     {
-        parent::__construct($node, $name, $arguments, $lineno);
-
         if ($node instanceof Twig_Node_Expression_Name) {
             $node->setAttribute('is_defined_test', true);
         } elseif ($node instanceof Twig_Node_Expression_GetAttr) {
             $node->setAttribute('is_defined_test', true);
 
             $this->changeIgnoreStrictCheck($node);
+        } elseif ($node instanceof Twig_Node_Expression_Constant || $node instanceof Twig_Node_Expression_Array) {
+            $node = new Twig_Node_Expression_Constant(true, $node->getLine());
         } else {
             throw new Twig_Error_Syntax('The "defined" test only works with simple variables.', $this->getLine());
         }
+
+        parent::__construct($node, $name, $arguments, $lineno);
     }
 
     protected function changeIgnoreStrictCheck(Twig_Node_Expression_GetAttr $node)
