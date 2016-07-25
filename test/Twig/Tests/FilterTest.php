52c6ec712e0be11959bf8ca2852d48fbef0cd58d
Sam Mousa <sam@mousa.nl>
2016-07-25 09:12:23 +0000

    Fixed coding standard issues.

--- test/Twig/Tests/FilterTest.php
+++ test/Twig/Tests/FilterTest.php
@@ -9,16 +9,13 @@ class Twig_Tests_FilterTest extends PHPUnit_Framework_TestCase
     {
         parent::setUp();
         $this->env = new Twig_Environment(new Twig_Loader_Array(array()));
-
     }
-
 
     public function sliceProvider()
     {
         return array(
             array(array('a', 'b'), 1, 1, false, array('b')),
-            array(array('a' => 'b', 'b' => 'c'), 1, 1, false, array('b' => 'c'))
-
+            array(array('a' => 'b', 'b' => 'c'), 1, 1, false, array('b' => 'c')),
 
         );
     }
@@ -27,11 +24,9 @@ class Twig_Tests_FilterTest extends PHPUnit_Framework_TestCase
     {
         return array(
             array(array('a', 'b'), array(0, 1)),
-            array(array('a' => 'b', 'b' => 'c'), array('a', 'b'))
-
+            array(array('a' => 'b', 'b' => 'c'), array('a', 'b')),
 
         );
-
     }
 
     /**
@@ -50,8 +45,5 @@ class Twig_Tests_FilterTest extends PHPUnit_Framework_TestCase
     public function testKeys($array, $expected)
     {
         $this->assertEquals($expected, twig_get_array_keys_filter($array));
-
     }
-
-
 }
