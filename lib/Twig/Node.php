9c2747996a00aec0b221a0e54c4178eaab83ce9a
Fabien Potencier <fabien.potencier@gmail.com>
2016-09-24 11:30:39 -0700

    added a missing deprecation notice

--- lib/Twig/Node.php
+++ lib/Twig/Node.php
@@ -213,6 +213,10 @@ class Twig_Node implements Twig_NodeInterface
      */
     public function setNode($name, $node = null)
     {
+        if (!$node instanceof Twig_NodeInterface) {
+            @trigger_error(sprintf('Using "%s" for the value of node "%s" of "%s" is deprecated since version 1.25 and will be removed in 2.0.', is_object($node) ? get_class($node) : null === $node ? 'null' : gettype($node), $name, get_class($this)), E_USER_DEPRECATED);
+        }
+
         $this->nodes[$name] = $node;
     }
 
