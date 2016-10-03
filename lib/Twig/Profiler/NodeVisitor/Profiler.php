8c5493928a49f2b711703eb350aa8822f4f1d6a3
oittaa <oittaa@users.noreply.github.com>
2016-10-03 21:35:59 +0300

    [Twig/Profiler/NodeVisitor/Profiler.php] Use twig_random_bytes

--- lib/Twig/Profiler/NodeVisitor/Profiler.php
+++ lib/Twig/Profiler/NodeVisitor/Profiler.php
@@ -59,7 +59,7 @@ class Twig_Profiler_NodeVisitor_Profiler extends Twig_BaseNodeVisitor
 
     private function getVarName()
     {
-        return sprintf('__internal_%s', hash('sha256', uniqid(mt_rand(), true), false));
+        return sprintf('__internal_%s', bin2hex(twig_random_bytes(32)));
     }
 
     /**
