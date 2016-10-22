124e27be3cf50e11273180438e977caf56e449ba
Kyle Gadd <kyle.gadd@gmail.com>
2016-10-21 16:20:33 -0800

    Update ExpressionParserTest.php

--- test/Twig/Tests/ExpressionParserTest.php
+++ test/Twig/Tests/ExpressionParserTest.php
@@ -88,15 +88,15 @@ class Twig_Tests_ExpressionParserTest extends PHPUnit_Framework_TestCase
                   new Twig_Node_Expression_Constant(2, 1),
                 ), 1),
             ),
-            array('{{ [1: "one", "two": 2, "three"] }}', new Twig_Node_Expression_Array(array(
+            array('{{ [1: "a", "b": 2, "c"] }}', new Twig_Node_Expression_Array(array(
                   new Twig_Node_Expression_Constant(1, 1),
-                  new Twig_Node_Expression_Constant('one', 1),
+                  new Twig_Node_Expression_Constant('a', 1),
 
-                  new Twig_Node_Expression_Constant('two', 1),
+                  new Twig_Node_Expression_Constant('b', 1),
                   new Twig_Node_Expression_Constant(2, 1),
 
                   new Twig_Node_Expression_Constant(0, 1),
-                  new Twig_Node_Expression_Constant('three', 1),
+                  new Twig_Node_Expression_Constant('c', 1),
                 ), 1),
             ), 
 
