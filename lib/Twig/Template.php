704637a912c57f232f5ccab437935e41e79c549e
SpacePossum <possumfromspace@gmail.com>
2016-11-16 11:46:56 +0100

    Update Template.php

--- lib/Twig/Template.php
+++ lib/Twig/Template.php
@@ -281,8 +281,8 @@ abstract class Twig_Template
      * This method checks blocks defined in the current template
      * or defined in "used" traits or defined in parent templates.
      *
-     * @param array  $context The context
-     * @param array  $blocks  The current set of blocks
+     * @param array $context The context
+     * @param array $blocks  The current set of blocks
      *
      * @return array An array of block names
      *
@@ -317,7 +317,7 @@ abstract class Twig_Template
             return $this->env->loadTemplate($template, $index);
         } catch (Twig_Error $e) {
             if (!$e->getTemplateName()) {
-                $e->setTemplateName($templateName ? : $this->getTemplateName());
+                $e->setTemplateName($templateName ?: $this->getTemplateName());
             }
 
             if ($e->getTemplateLine()) {
