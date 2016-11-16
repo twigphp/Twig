a4c179ad5385a3cb28f7e3ec59a895567a09ffc0
SpacePossum <possumfromspace@gmail.com>
2016-11-16 10:07:44 +0100

    Update Power.php

--- lib/Twig/Node/Expression/Binary/Power.php
+++ lib/Twig/Node/Expression/Binary/Power.php
@@ -10,15 +10,6 @@
  */
 class Twig_Node_Expression_Binary_Power extends Twig_Node_Expression_Binary
 {
-    public function compile(Twig_Compiler $compiler)
-    {
-        $compiler
-            ->subcompile($this->getNode('left'))
-            ->raw(' ** ')
-            ->subcompile($this->getNode('right'))
-        ;
-    }
-
     public function operator(Twig_Compiler $compiler)
     {
         return $compiler->raw('**');
