a905a68d38298d234d283ffa28e919e60459981f
SpacePossum <possumfromspace@gmail.com>
2016-11-17 15:49:43 +0100

    Update TemplateWrapper.php

--- lib/Twig/TemplateWrapper.php
+++ lib/Twig/TemplateWrapper.php
@@ -102,26 +102,7 @@ final class Twig_TemplateWrapper
      */
     public function displayBlock($name, $context = array())
     {
-        $context = $this->env->mergeGlobals($context);
-        $level = ob_get_level();
-        ob_start();
-        try {
-            $this->template->displayBlock($name, $context);
-        } catch (Exception $e) {
-            while (ob_get_level() > $level) {
-                ob_end_clean();
-            }
-
-            throw $e;
-        } catch (Throwable $e) {
-            while (ob_get_level() > $level) {
-                ob_end_clean();
-            }
-
-            throw $e;
-        }
-
-        ob_get_clean();
+        $this->template->displayBlock($name, $this->env->mergeGlobals($context));
     }
 
     /**
