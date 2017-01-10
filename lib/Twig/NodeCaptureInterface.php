44eba84e2414e55a8ec60002598036291936656e
Fabien Potencier <fabien.potencier@gmail.com>
2017-01-10 14:02:12 -0800

    fixed typo

--- lib/Twig/NodeCaptureInterface.php
+++ lib/Twig/NodeCaptureInterface.php
@@ -3,14 +3,14 @@
 /*
  * This file is part of Twig.
  *
- * (c) 2010 Fabien Potencier
+ * (c) Fabien Potencier
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
 /**
- * Represents a node that will capture any nested displayable nodes.
+ * Represents a node that captures any nested displayable nodes.
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
