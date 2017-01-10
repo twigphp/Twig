3cdbd325d42588e1b4d6eacd0b062f00cc3c8903
Fabien Potencier <fabien.potencier@gmail.com>
2017-01-10 14:00:36 -0800

    bumped version to 1.31.0

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.30.1-DEV"
+#define PHP_TWIG_VERSION "1.31.0-DEV"
 
 #include "php.h"
 
--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,11 +16,11 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.30.1-DEV';
-    const VERSION_ID = 13001;
+    const VERSION = '1.31.0-DEV';
+    const VERSION_ID = 13100;
     const MAJOR_VERSION = 1;
-    const MINOR_VERSION = 30;
-    const RELEASE_VERSION = 1;
+    const MINOR_VERSION = 31;
+    const RELEASE_VERSION = 0;
     const EXTRA_VERSION = 'DEV';
 
     protected $charset;
