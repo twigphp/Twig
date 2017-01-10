4e1c232b67c577a97407133975d8caf2321efd30
Fabien Potencier <fabien.potencier@gmail.com>
2017-01-10 14:14:10 -0800

    removed years in (c) in files (we have them in the referenced main LICENSE file)

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -8,7 +8,7 @@ if (!defined('ENT_SUBSTITUTE')) {
 /*
  * This file is part of Twig.
  *
- * (c) 2009 Fabien Potencier
+ * (c) Fabien Potencier
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
