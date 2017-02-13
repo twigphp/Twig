c7c979e4fd0b20ebb73d3b76117288c710eeb667
SpacePossum <possumfromspace@gmail.com>
2017-02-13 13:29:52 +0100

    Deprecate warnings and upgrade path.

--- lib/Twig/SimpleFilter.php
+++ lib/Twig/SimpleFilter.php
@@ -12,6 +12,15 @@
 /**
  * Empty class for Twig 1.x compatibility.
  */
-final class Twig_SimpleFilter extends Twig_Filter
+class Twig_SimpleFilter extends Twig_Filter
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(string $name, $callable = null, array $options = array())
+    {
+        @trigger_error('"Twig_SimpleFilter" is deprecated in favor of "Twig_Filter" and will be removed in Twig 3.0.', E_USER_DEPRECATED);
+
+        parent::__construct($name, $callable, $options);
+    }
 }
