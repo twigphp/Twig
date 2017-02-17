f63c6e1a9aa4adfecea289d3bc3accdd1128e82f
Fabien Potencier <fabien.potencier@gmail.com>
2017-02-17 11:26:43 -0800

    fixed tests for PHP 5.2

--- test/Twig/Tests/ContainerRuntimeLoaderTest.php
+++ test/Twig/Tests/ContainerRuntimeLoaderTest.php
@@ -18,7 +18,7 @@ class Twig_Tests_ContainerRuntimeLoaderTest extends PHPUnit_Framework_TestCase
     {
         $container = $this->getMockBuilder('Psr\Container\ContainerInterface')->getMock();
         $container->expects($this->once())->method('has')->with('stdClass')->willReturn(true);
-        $container->expects($this->once())->method('get')->with('stdClass')->willReturn(new \Stdclass());
+        $container->expects($this->once())->method('get')->with('stdClass')->willReturn(new Stdclass());
 
         $loader = new Twig_ContainerRuntimeLoader($container);
 
@@ -31,6 +31,7 @@ class Twig_Tests_ContainerRuntimeLoaderTest extends PHPUnit_Framework_TestCase
         $container->expects($this->once())->method('has')->with('Foo');
         $container->expects($this->never())->method('get');
 
-        $this->assertNull((new Twig_ContainerRuntimeLoader($container))->load('Foo'));
+        $loader = new Twig_ContainerRuntimeLoader($container);
+        $this->assertNull($loader->load('Foo'));
     }
 }
