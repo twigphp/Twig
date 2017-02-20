89885d1bc6aecbdc0814eb5559dc3df1555af70e
antonydandrea <antony492@yahoo.co.uk>
2017-02-20 10:20:30 +0000

    reversed last commit. If person puts empty string as the first parameter, it would override the default character mask

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -860,8 +860,11 @@ function twig_in_filter($value, $compare)
  * @param string $mode
  * @return string
  */
-function twig_trim_filter($string, $characterMask = " \t\n\r\0\x0B", $mode = 'both')
+function twig_trim_filter($string, $characterMask = null, $mode = 'both')
 {
+    if (empty($characterMask)) {
+        $characterMask = " \t\n\r\0\x0B";
+    }
     switch ($mode) {
         case 'left':
             $trimmed = ltrim($string, $characterMask);
