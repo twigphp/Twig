a5c2da6de0f126a86ba999755a9ac34c1ffae954
Matthias Pigulla <mp@webfactory.de>
2017-03-24 15:59:34 +0100

    test if is_object before calling method_exists, fixes #2431

--- lib/Twig/Extension/Core.php
+++ lib/Twig/Extension/Core.php
@@ -1425,7 +1425,7 @@ function twig_test_empty($value)
         return 0 == count($value);
     }
 
-    if (method_exists($value, '__toString')) {
+    if (is_object($value) && method_exists($value, '__toString')) {
         return '' === (string) $value;
     }
 
