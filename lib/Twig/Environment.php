e3bdc12cba135d632a5bb2bcf23717c7af65ae2c
Fabien Potencier <fabien.potencier@gmail.com>
2017-06-05 10:24:44 -0700

    prepared the 2.4.0 release

--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,12 +16,12 @@
  */
 class Twig_Environment
 {
-    const VERSION = '2.4.0-DEV';
+    const VERSION = '2.4.0';
     const VERSION_ID = 20400;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 4;
     const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     private $charset;
     private $loader;
