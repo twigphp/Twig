5aad21c1e7492ea9d87e51f64dd81cc6becccb8e
Fabien Potencier <fabien.potencier@gmail.com>
2017-06-07 11:48:31 -0700

    bumped version to 2.4.4-DEV

--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,12 +16,12 @@
  */
 class Twig_Environment
 {
-    const VERSION = '2.4.3';
-    const VERSION_ID = 20403;
+    const VERSION = '2.4.4-DEV';
+    const VERSION_ID = 20404;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 4;
-    const RELEASE_VERSION = 3;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 4;
+    const EXTRA_VERSION = 'DEV';
 
     private $charset;
     private $loader;
