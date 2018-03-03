9c24f2cd39dc1906b76879e099970b7e53724601
Fabien Potencier <fabien.potencier@gmail.com>
2018-03-03 08:21:29 -0800

    prepared the 1.35.2 release

--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,12 +16,12 @@
  */
 class Twig_Environment
 {
-    const VERSION = '1.35.2-DEV';
+    const VERSION = '1.35.2';
     const VERSION_ID = 13501;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 35;
     const RELEASE_VERSION = 1;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     protected $charset;
     protected $loader;
