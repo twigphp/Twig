bf7bbf1e9371801240448387ee5a9e53f9157913
Fabien Potencier <fabien.potencier@gmail.com>
2018-03-03 08:24:39 -0800

    fixed version

--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -17,10 +17,10 @@
 class Twig_Environment
 {
     const VERSION = '1.35.3-DEV';
-    const VERSION_ID = 13501;
+    const VERSION_ID = 13503;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 35;
-    const RELEASE_VERSION = 1;
+    const RELEASE_VERSION = 3;
     const EXTRA_VERSION = 'DEV';
 
     protected $charset;
