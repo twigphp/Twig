27f905edbe3ed6c599cdbd1449e3ce670da749a6
Fabien Potencier <fabien.potencier@gmail.com>
2018-03-04 08:46:32 -0800

    made a small performance improvement

--- lib/Twig/ExtensionSet.php
+++ lib/Twig/ExtensionSet.php
@@ -160,6 +160,8 @@ final class Twig_ExtensionSet
             throw new LogicException(sprintf('Unable to register extension "%s" as it is already registered.', $class));
         }
 
+        // For BC/FC with namespaced aliases
+        $class = (new ReflectionClass($class))->name;
         $this->extensions[$class] = $extension;
     }
 
