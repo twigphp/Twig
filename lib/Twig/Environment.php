cc5c649af60b9f05d79661d56ff509dea6b79222
Fabien Potencier <fabien@potencier.org>
2018-12-16 11:37:16 +0100

    bumped version to 2.6.1-DEV

--- lib/Twig/Environment.php
+++ lib/Twig/Environment.php
@@ -16,12 +16,12 @@
  */
 class Twig_Environment
 {
-    const VERSION = '2.6.0';
-    const VERSION_ID = 20600;
+    const VERSION = '2.6.1-DEV';
+    const VERSION_ID = 20601;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 6;
-    const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 1;
+    const EXTRA_VERSION = 'DEV';
 
     private $charset;
     private $loader;
