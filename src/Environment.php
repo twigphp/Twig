57bd838bb7a9368ecf8b19bbe9788090502d1615
Fabien Potencier <fabien@potencier.org>
2019-03-12 13:46:23 +0100

    prepared the 2.7.0 release

--- src/Environment.php
+++ src/Environment.php
@@ -38,12 +38,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '2.7.0-DEV';
+    const VERSION = '2.7.0';
     const VERSION_ID = 20700;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 7;
     const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     private $charset;
     private $loader;
