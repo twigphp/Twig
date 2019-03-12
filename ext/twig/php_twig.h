874adbd9222f928f6998732b25b01b41dff15b0c
Fabien Potencier <fabien@potencier.org>
2019-03-12 19:45:24 +0100

    prepared the 1.38.2 release

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.38.2-DEV"
+#define PHP_TWIG_VERSION "1.38.2"
 
 #include "php.h"
 
--- src/Environment.php
+++ src/Environment.php
@@ -41,12 +41,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '1.38.2-DEV';
+    const VERSION = '1.38.2';
     const VERSION_ID = 13802;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 38;
     const RELEASE_VERSION = 2;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     protected $charset;
     protected $loader;
