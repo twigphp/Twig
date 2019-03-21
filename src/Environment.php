5df0ef0ca2d08a6f087384c7f31530fb84889f28
Fabien Potencier <fabien@potencier.org>
2019-03-21 15:50:13 +0100

    prepared the 1.38.3 release

--- src/Environment.php
+++ src/Environment.php
@@ -41,12 +41,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '1.38.3-DEV';
+    const VERSION = '1.38.3';
     const VERSION_ID = 13803;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 38;
     const RELEASE_VERSION = 3;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     protected $charset;
     protected $loader;
