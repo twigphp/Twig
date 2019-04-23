802646aa5ea6d0e793d2baca3425107d403020c2
Fabien Potencier <fabien@potencier.org>
2019-04-23 18:29:40 +0200

    removed Template::__toString()

--- src/Template.php
+++ src/Template.php
@@ -48,14 +48,6 @@ abstract class Template
     }
 
     /**
-     * @internal this method will be removed in 3.0 and is only used internally to provide an upgrade path from 1.x to 2.0
-     */
-    public function __toString()
-    {
-        return $this->getTemplateName();
-    }
-
-    /**
      * Returns the template name.
      *
      * @return string The template name
