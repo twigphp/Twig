b03f07e96124d0e77e21c651e616616d39c91b1a
Fabien Potencier <fabien@potencier.org>
2019-05-14 13:59:47 +0200

    bumped version to 1.41.1-DEV

--- ext/twig/php_twig.h
+++ ext/twig/php_twig.h
@@ -15,7 +15,7 @@
 #ifndef PHP_TWIG_H
 #define PHP_TWIG_H
 
-#define PHP_TWIG_VERSION "1.41.0"
+#define PHP_TWIG_VERSION "1.41.1-DEV"
 
 #include "php.h"
 
--- src/Environment.php
+++ src/Environment.php
@@ -41,12 +41,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '1.41.0';
-    const VERSION_ID = 14100;
+    const VERSION = '1.41.1-DEV';
+    const VERSION_ID = 14101;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 41;
-    const RELEASE_VERSION = 0;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 1;
+    const EXTRA_VERSION = 'DEV';
 
     protected $charset;
     protected $loader;
