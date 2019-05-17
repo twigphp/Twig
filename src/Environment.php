0f5a05696812575715d82e6eb4f07b353ce9ab06
Fabien Potencier <fabien@potencier.org>
2019-05-17 08:22:23 +0200

    bumped version to 1.42

--- src/Environment.php
+++ src/Environment.php
@@ -41,11 +41,11 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '1.41.1-DEV';
-    const VERSION_ID = 14101;
+    const VERSION = '1.42.0-DEV';
+    const VERSION_ID = 14200;
     const MAJOR_VERSION = 1;
-    const MINOR_VERSION = 41;
-    const RELEASE_VERSION = 1;
+    const MINOR_VERSION = 42;
+    const RELEASE_VERSION = 0;
     const EXTRA_VERSION = 'DEV';
 
     protected $charset;
