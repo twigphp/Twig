21707d6ebd05476854805e4f91b836531941bcd4
Fabien Potencier <fabien@potencier.org>
2019-06-18 17:34:59 +0200

    prepared the 1.42.2 release

--- src/Environment.php
+++ src/Environment.php
@@ -41,12 +41,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '1.42.2-DEV';
+    const VERSION = '1.42.2';
     const VERSION_ID = 14202;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 42;
     const RELEASE_VERSION = 2;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     protected $charset;
     protected $loader;
