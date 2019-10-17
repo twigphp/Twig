ddd4134af9bfc6dba4eff7c8447444ecc45b9ee5
Fabien Potencier <fabien@potencier.org>
2019-10-17 09:34:53 +0200

    Prepare the 2.12.1 release

--- src/Environment.php
+++ src/Environment.php
@@ -38,12 +38,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '2.12.1-DEV';
+    const VERSION = '2.12.1';
     const VERSION_ID = 21201;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 12;
     const RELEASE_VERSION = 1;
-    const EXTRA_VERSION = 'DEV';
+    const EXTRA_VERSION = '';
 
     private $charset;
     private $loader;
