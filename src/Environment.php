41a87b972972c81d35fe9718e9432c659e48b6dd
Fabien Potencier <fabien@potencier.org>
2019-12-28 08:14:03 +0100

    Bump version to 2.12.4-DEV

--- src/Environment.php
+++ src/Environment.php
@@ -38,12 +38,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '2.12.3';
-    const VERSION_ID = 21203;
+    const VERSION = '2.12.4-DEV';
+    const VERSION_ID = 21204;
     const MAJOR_VERSION = 2;
     const MINOR_VERSION = 12;
-    const RELEASE_VERSION = 3;
-    const EXTRA_VERSION = '';
+    const RELEASE_VERSION = 4;
+    const EXTRA_VERSION = 'DEV';
 
     private $charset;
     private $loader;
