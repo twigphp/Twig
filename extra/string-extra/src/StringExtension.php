f6b72e9c03d37988cb51579f19a16e9c4753906e
seb-jean <sebastien.jean76@gmail.com>
2020-08-12 10:25:32 +0200

    Update StringExtension.php

--- extra/string-extra/src/StringExtension.php
+++ extra/string-extra/src/StringExtension.php
@@ -11,6 +11,7 @@
 
 namespace Twig\Extra\String;
 
+use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Component\String\UnicodeString;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
@@ -21,11 +22,17 @@ final class StringExtension extends AbstractExtension
     {
         return [
             new TwigFilter('u', [$this, 'createUnicodeString']),
+            new TwigFilter('slug', [$this, 'createSlug']),
         ];
     }
 
     public function createUnicodeString(?string $text): UnicodeString
     {
         return new UnicodeString($text ?? '');
+    }
+
+    public function createSlug(?string $string, ?string $separator = '-', string $locale = null): string
+    {
+        return (new AsciiSlugger())->slug($string, $separator, $locale);
     }
 }
