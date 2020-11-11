d8a4113a74a82ac1be0bfb5666de72e5c911de74
Youssef Benhssaien <youssefbenhssaien@gmail.com>
2020-11-11 07:27:35 +0100

    Remove is_int since typehint exists

--- src/NodeVisitor/OptimizerNodeVisitor.php
+++ src/NodeVisitor/OptimizerNodeVisitor.php
@@ -51,7 +51,7 @@ final class OptimizerNodeVisitor implements NodeVisitorInterface
      */
     public function __construct(int $optimizers = -1)
     {
-        if (!\is_int($optimizers) || $optimizers > (self::OPTIMIZE_FOR | self::OPTIMIZE_RAW_FILTER)) {
+        if ($optimizers > (self::OPTIMIZE_FOR | self::OPTIMIZE_RAW_FILTER)) {
             throw new \InvalidArgumentException(sprintf('Optimizer mode "%s" is not valid.', $optimizers));
         }
 
