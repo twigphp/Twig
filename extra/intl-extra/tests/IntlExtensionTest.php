9c8dc58b2af898eb1c7c3e88527beea26f64a399
Fran Moreno <franmomu@gmail.com>
2020-12-02 09:06:47 +0100

    Add test showing currency behaviour

--- extra/intl-extra/tests/IntlExtensionTest.php
+++ extra/intl-extra/tests/IntlExtensionTest.php
@@ -25,4 +25,23 @@ class IntlExtensionTest extends TestCase
         $ext = new IntlExtension($dateFormatterProto, $numberFormatterProto);
         $this->assertSame('++12,3', $ext->formatNumber('12.3456'));
     }
+
+    /**
+     * @dataProvider getCurrencyData
+     */
+    public function testFormatterProtoWithCurrency(string $locale, string $expectedCurrency)
+    {
+        $numberFormatterProto = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
+        $numberFormatterProto->setAttribute(\NumberFormatter::FRACTION_DIGITS, 1);
+        $ext = new IntlExtension(null, $numberFormatterProto);
+        $this->assertSame($expectedCurrency, $ext->formatCurrency('12.3456', 'EUR'));
+    }
+
+    public function getCurrencyData()
+    {
+        return [
+            ['en', '€12.3'],
+            ['fr', '12,3 €'],
+        ];
+    }
 }
