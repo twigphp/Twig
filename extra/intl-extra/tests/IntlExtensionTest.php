a51517adb9e3f851e78526d20e94e11ee2e6bdd2
Fran Moreno <franmomu@gmail.com>
2020-12-04 21:58:00 +0100

    Testing solution

--- extra/intl-extra/tests/IntlExtensionTest.php
+++ extra/intl-extra/tests/IntlExtensionTest.php
@@ -29,19 +29,28 @@ class IntlExtensionTest extends TestCase
     /**
      * @dataProvider getCurrencyData
      */
-    public function testFormatterProtoWithCurrency(string $locale, string $expectedCurrency)
+    public function testFormatterProtoWithCurrency(string $locale, string $currency, string $expectedCurrency)
     {
         $numberFormatterProto = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
         $numberFormatterProto->setAttribute(\NumberFormatter::FRACTION_DIGITS, 1);
         $ext = new IntlExtension(null, $numberFormatterProto);
-        $this->assertSame($expectedCurrency, $ext->formatCurrency('12.3456', 'EUR'));
+        $this->assertSame($expectedCurrency, $this->fixSpace($ext->formatCurrency('12.3456', $currency, [], $locale)));
     }
 
     public function getCurrencyData()
     {
         return [
-            ['en', '€12.3'],
-            ['fr', '12,3 €'],
+            ['en', 'EUR', '€12.3'],
+            ['fr', 'EUR', '12,3 €'],
+            ['jp', 'YEN', 'YEN 12.3'],
         ];
+    }
+
+    private function fixSpace(string $string): string
+    {
+        $string = mb_convert_encoding($string, 'HTML-ENTITIES', 'UTF-8');
+        $stringWithoutSpaces = str_replace('&nbsp;', ' ', $string);
+
+        return html_entity_decode($stringWithoutSpaces);
     }
 }
