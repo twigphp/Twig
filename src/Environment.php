8442df056c51b706793adf80a9fd363406dd3674
Fabien Potencier <fabien@potencier.org>
2022-04-06 08:47:41 +0200

    Prepare the 2.14.13 release

--- src/Environment.php
+++ src/Environment.php
@@ -38,12 +38,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '3.3.10-DEV';
+    public const VERSION = '3.3.10';
     public const VERSION_ID = 30310;
     public const MAJOR_VERSION = 3;
     public const MINOR_VERSION = 3;
     public const RELEASE_VERSION = 10;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     private $charset;
     private $loader;
