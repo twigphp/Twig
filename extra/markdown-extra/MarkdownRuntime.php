ec9d56514bc64196047fd95e29eec7bd49785d66
Peter Stalman <sarkedev@gmail.com>
2022-04-19 10:14:32 -0700

    Fix for twigphp/Twig/issues/3685 (markdown leading linebreaks and indentation)

--- extra/markdown-extra/MarkdownRuntime.php
+++ extra/markdown-extra/MarkdownRuntime.php
@@ -22,11 +22,37 @@ class MarkdownRuntime
 
     public function convert(string $body): string
     {
-        // remove indentation
-        if ($white = substr($body, 0, strspn($body, " \t\r\n\0\x0B"))) {
-            $body = preg_replace("{^$white}m", '', $body);
-        }
+        $body = $this->commonWhitespace($body);
+        $body = $this->removeIndentation($body);
 
         return $this->converter->convert($body);
+    }
+
+    protected function commonWhitespace(string $body): string
+    {
+        return str_replace(["\t", "\0", "\x0B"], ['    ', '', ''], $body);
+    }
+
+    protected function removeIndentation(string $body): string
+    {
+        $indent = $this->minIndentations($body);
+        if ($indent > 0) {
+            $body = preg_replace("{^ {{$indent}}}m", '', $body);
+        }
+
+        return $body;
+    }
+
+    protected function minIndentations(string $body): int
+    {
+        $non_empty_lines = preg_split('%(\r|\n)%', $body, -1, PREG_SPLIT_NO_EMPTY);
+
+        $list = [];
+        foreach ($non_empty_lines as $line)
+        {
+            $list[] = strspn($line, " ");
+        }
+
+        return min($list);
     }
 }
