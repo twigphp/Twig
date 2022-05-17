e939eae92386b69b49cfa4599dd9bead6bf4a342
Fabien Potencier <fabien@potencier.org>
2022-05-17 07:48:52 +0200

    Prepare the 3.4.1 release

--- src/Environment.php
+++ src/Environment.php
@@ -38,12 +38,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '3.4.1-DEV';
+    public const VERSION = '3.4.1';
     public const VERSION_ID = 30401;
     public const MAJOR_VERSION = 3;
     public const MINOR_VERSION = 4;
     public const RELEASE_VERSION = 1;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     private $charset;
     private $loader;
