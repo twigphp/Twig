e61754530826c400c5c9d283ff352dd5bf548152
Nick Farley <nick@nfreader.net>
2022-07-16 09:00:24 -0500

    Applying cs fix from fabpot.io

--- src/Extension/DebugExtension.php
+++ src/Extension/DebugExtension.php
@@ -19,10 +19,10 @@ final class DebugExtension extends AbstractExtension
         // dump is safe if var_dump is overridden by xdebug
         $isDumpOutputHtmlSafe = \extension_loaded('xdebug')
             // false means that it was not set (and the default is on) or it explicitly enabled
-            && (false === ini_get('xdebug.overload_var_dump') || ini_get('xdebug.overload_var_dump'))
+            && (false === \ini_get('xdebug.overload_var_dump') || \ini_get('xdebug.overload_var_dump'))
             // false means that it was not set (and the default is on) or it explicitly enabled
             // xdebug.overload_var_dump produces HTML only when html_errors is also enabled
-            && (false === ini_get('html_errors') || ini_get('html_errors'))
+            && (false === \ini_get('html_errors') || \ini_get('html_errors'))
             || 'cli' === \PHP_SAPI
         ;
 
