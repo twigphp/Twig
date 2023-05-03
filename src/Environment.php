3fd3645601085084f808577b870e5e7e3569c1c6
Fabien Potencier <fabien@potencier.org>
2023-05-03 21:08:22 +0200

    Bump version

--- src/Environment.php
+++ src/Environment.php
@@ -40,12 +40,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '3.6.0';
-    public const VERSION_ID = 30600;
+    public const VERSION = '3.6.1-DEV';
+    public const VERSION_ID = 30601;
     public const MAJOR_VERSION = 3;
-    public const MINOR_VERSION = 3;
-    public const RELEASE_VERSION = 0;
-    public const EXTRA_VERSION = '';
+    public const MINOR_VERSION = 6;
+    public const RELEASE_VERSION = 1;
+    public const EXTRA_VERSION = 'DEV';
 
     private $charset;
     private $loader;
