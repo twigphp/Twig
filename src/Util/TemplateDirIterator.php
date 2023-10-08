41f1b0370a4bddcb4fa03f7a7556683a1050aaeb
Christian Flothmann <christian.flothmann@qossmic.com>
2023-10-08 20:59:56 +0200

    restore return type annotations

--- src/Util/TemplateDirIterator.php
+++ src/Util/TemplateDirIterator.php
@@ -16,12 +16,18 @@ namespace Twig\Util;
  */
 class TemplateDirIterator extends \IteratorIterator
 {
+    /**
+     * @return mixed
+     */
     #[\ReturnTypeWillChange]
     public function current()
     {
         return file_get_contents(parent::current());
     }
 
+    /**
+     * @return mixed
+     */
     #[\ReturnTypeWillChange]
     public function key()
     {
