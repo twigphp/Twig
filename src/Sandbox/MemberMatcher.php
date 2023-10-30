2ee0fad6ef713c32baf959f94616afdcd6e55674
Yaakov Saxon <ysaxon@nabancard.com>
2023-10-30 15:17:14 -0400

    minor fabbot changes

--- src/Sandbox/MemberMatcher.php
+++ src/Sandbox/MemberMatcher.php
@@ -16,7 +16,7 @@ namespace Twig\Sandbox;
  * - Class can be specified as wildcard `* => [...]` in order to allow those methods/properties for all classes.
  * - Method/property can be specified as wildcard eg. `\DateTime => '*'` in order to allow all methods/properties for that class.
  * - Method/property can also be specified with a trailing wildcard to allow all methods/properties with a certain prefix, eg. `\DateTime => ['get*', ...]` in order to allow all methods/properties that start with `get`.
- * 
+ *
  * @author Yaakov Saxon <ysaxon@gmail.com>
  */
 final class MemberMatcher
@@ -28,16 +28,16 @@ final class MemberMatcher
     {
         $normalizedMembers = [];
         foreach ($allowedMembers as $class => $members) {
-            if (!is_array($members)) {
+            if (!\is_array($members)) {
                 $normalizedMembers[$class][] = strtolower($members);
-            }
-            else foreach ($members as $index => $member) {
-                $normalizedMembers[$class][$index] = strtolower($member);
+            } else {
+                foreach ($members as $index => $member) {
+                    $normalizedMembers[$class][$index] = strtolower($member);
+                }
             }
         }
         $this->allowedMembers = $normalizedMembers;
     }
-
 
     public function isAllowed($obj, string $member): bool
     {
@@ -51,19 +51,22 @@ final class MemberMatcher
         $member = strtolower($member); // normalize member name
 
         foreach ($this->allowedMembers as $class => $members) {
-            if ($class === '*' || $obj instanceof $class) {
+            if ('*' === $class || $obj instanceof $class) {
                 foreach ($members as $allowedMember) {
-                    if ($allowedMember === '*') {
+                    if ('*' === $allowedMember) {
                         $this->cache[$cacheKey] = true;
+
                         return true;
                     }
                     if ($allowedMember === $member) {
                         $this->cache[$cacheKey] = true;
+
                         return true;
                     }
-                    //if allowedMember ends with a *, check if the member starts with the allowedMember
-                    if (substr($allowedMember, -1) === '*' && substr($member, 0, strlen($allowedMember) - 1) === rtrim($allowedMember, '*')) {
+                    // if allowedMember ends with a *, check if the member starts with the allowedMember
+                    if ('*' === substr($allowedMember, -1) && substr($member, 0, \strlen($allowedMember) - 1) === rtrim($allowedMember, '*')) {
                         $this->cache[$cacheKey] = true;
+
                         return true;
                     }
                 }
