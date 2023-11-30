29f3b3ea4aa433912c0aad8a5c723fe844f9c8f8
Jérôme Tamarelle <jerome@tamarelle.net>
2023-11-30 09:47:58 +0100

    Update phpdoc

--- src/Extension/Attribute/AsTwigFilter.php
+++ src/Extension/Attribute/AsTwigFilter.php
@@ -1,11 +1,27 @@
 <?php
 
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Twig\Extension\Attribute;
 
 use Twig\TwigFilter;
 
 /**
  * Registers a method as template filter.
+ *
+ * If the first argument of the method has Twig\Environment type-hint, the filter will receive the current environment.
+ * If the next argument of the method is named $context and has array type-hint, the filter will receive the current context.
+ * Additional arguments of the method come from the filter call.
+ *
+ *     #[AsTwigFilter('foo')]
+ *     function fooFilter(Environment $env, array $context, $string, $arg1 = null, ...) { ... }
  *
  * @see TwigFilter
  */
@@ -14,7 +30,7 @@ class AsTwigFilter
 {
     public function __construct(
         /**
-         * The name of the filter in Twig
+         * The name of the filter in Twig.
          *
          * @var non-empty-string $name
          */
--- src/Extension/Attribute/AsTwigTest.php
+++ src/Extension/Attribute/AsTwigTest.php
@@ -1,11 +1,27 @@
 <?php
 
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Twig\Extension\Attribute;
 
 use Twig\TwigTest;
 
 /**
  * Registers a method as template test.
+ *
+ * If the first argument of the method has Twig\Environment type-hint, the test will receive the current environment.
+ * If the next argument of the method is named $context and has array type-hint, the test will receive the current context.
+ * The last argument of the method is the value to be tested, if any.
+ *
+ *     #[AsTwigTest('foo')]
+ *     public function fooTest(Environment $env, array $context, $value, $arg1 = null) { ... }
  *
  * @see TwigTest
  */
