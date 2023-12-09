69a89e0e77b101ac414022833d12a509f137304a
Fabien Potencier <fabien@potencier.org>
2023-12-09 18:24:26 +0100

    Move functions for MarkdownExtension

--- extra/markdown-extra/MarkdownExtension.php
+++ extra/markdown-extra/MarkdownExtension.php
@@ -21,28 +21,31 @@ final class MarkdownExtension extends AbstractExtension
     {
         return [
             new TwigFilter('markdown_to_html', ['Twig\\Extra\\Markdown\\MarkdownRuntime', 'convert'], ['is_safe' => ['all']]),
-            new TwigFilter('html_to_markdown', 'Twig\\Extra\\Markdown\\twig_html_to_markdown', ['is_safe' => ['all']]),
+            new TwigFilter('html_to_markdown', [self::class, 'htmlToMarkdown'], ['is_safe' => ['all']]),
         ];
     }
-}
 
-function twig_html_to_markdown(string $body, array $options = []): string
-{
-    static $converters;
-
-    if (!class_exists(HtmlConverter::class)) {
-        throw new \LogicException('You cannot use the "html_to_markdown" filter as league/html-to-markdown is not installed; try running "composer require league/html-to-markdown".');
+    /**
+     * @internal
+     */
+    public static function htmlToMarkdown(string $body, array $options = []): string
+    {
+        static $converters;
+    
+        if (!class_exists(HtmlConverter::class)) {
+            throw new \LogicException('You cannot use the "html_to_markdown" filter as league/html-to-markdown is not installed; try running "composer require league/html-to-markdown".');
+        }
+    
+        $options += [
+            'hard_break' => true,
+            'strip_tags' => true,
+            'remove_nodes' => 'head style',
+        ];
+    
+        if (!isset($converters[$key = serialize($options)])) {
+            $converters[$key] = new HtmlConverter($options);
+        }
+    
+        return $converters[$key]->convert($body);
     }
-
-    $options += [
-        'hard_break' => true,
-        'strip_tags' => true,
-        'remove_nodes' => 'head style',
-    ];
-
-    if (!isset($converters[$key = serialize($options)])) {
-        $converters[$key] = new HtmlConverter($options);
-    }
-
-    return $converters[$key]->convert($body);
 }
---
+++ extra/markdown-extra/Tests/LegacyFunctionsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Twig\Extra\Markdown\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Twig\Extra\Markdown\MarkdownExtension;
+use function Twig\Extra\Markdown\html_to_markdown;
+
+/**
+ * @group legacy
+ */
+class LegacyFunctionsTest extends TestCase
+{
+    public function testHtmlToMarkdown()
+    {
+        $this->assertSame(MarkdownExtension::htmlToMarkdown('<p>foo</p>'), html_to_markdown('<p>foo</p>'));
+    }
+}
