6a18cda5aaad6fff27ebe32c6f753358837a4faa
Fabien Potencier <fabien@potencier.org>
2023-12-09 18:04:05 +0100

    Move functions for HtmlExtension

---
+++ extra/html-extra/Tests/LegacyFunctionsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Twig\Extra\Html\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Twig\Extra\Html\HtmlExtension;
+
+/**
+ * @group legacy
+ */
+class LegacyFunctionsTest extends TestCase
+{
+    public function testHtmlToMarkdown()
+    {
+        $this->assertSame(HtmlExtension::htmlClasses(['charset' => 'utf-8']), \twig_html_classes(['charset' => 'utf-8']));
+    }
+}
