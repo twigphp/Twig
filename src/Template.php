37b155b0921a3e2904e79160d682c065cb8afb4e
Fabien Potencier <fabien@potencier.org>
2023-12-13 13:40:03 +0100

    Add type hints

--- src/Template.php
+++ src/Template.php
@@ -299,7 +299,7 @@ abstract class Template
     /**
      * @return Template|TemplateWrapper
      */
-    protected function loadTemplate($template, $templateName = null, $line = null, $index = null)
+    protected function loadTemplate(string|TemplateWrapper|array $template, string $templateName = null, int $line = null, int $index = null)
     {
         try {
             if (\is_array($template)) {
