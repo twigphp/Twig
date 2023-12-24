6e2f2655a88a84ccd03cd030641666c0c9caa19b
Fabien Potencier <fabien@potencier.org>
2023-12-24 09:54:44 +0100

    Add missing dots at the end of exception messages

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -272,7 +272,7 @@ class TwigTestExtension extends AbstractExtension
     public function __call($method, $arguments)
     {
         if ('magicCall' !== $method) {
-            throw new \BadMethodCallException('Unexpected call to __call');
+            throw new \BadMethodCallException('Unexpected call to __call.');
         }
 
         return 'magic_'.$arguments[0];
@@ -281,7 +281,7 @@ class TwigTestExtension extends AbstractExtension
     public static function __callStatic($method, $arguments)
     {
         if ('magicStaticCall' !== $method) {
-            throw new \BadMethodCallException('Unexpected call to __callStatic');
+            throw new \BadMethodCallException('Unexpected call to __callStatic.');
         }
 
         return 'static_magic_'.$arguments[0];
@@ -296,7 +296,7 @@ class MagicCallStub
 {
     public function __call($name, $args)
     {
-        throw new \Exception('__call shall not be called');
+        throw new \Exception('__call shall not be called.');
     }
 }
 
@@ -339,7 +339,7 @@ class CountableStub implements \Countable
 
     public function __toString()
     {
-        throw new \Exception('__toString shall not be called on \Countables');
+        throw new \Exception('__toString shall not be called on \Countables.');
     }
 }
 
