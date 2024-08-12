1da763408b3cf540d7d62b6734676f1566906d69
Fabien Potencier <fabien@potencier.org>
2024-08-12 10:49:48 +0200

    Simplify code

--- src/Node/Expression/Filter/RawFilter.php
+++ src/Node/Expression/Filter/RawFilter.php
@@ -23,14 +23,7 @@ class RawFilter extends FilterExpression
 {
     public function __construct(Node $node, ?ConstantExpression $filterName = null, ?Node $arguments = null, int $lineno = 0, ?string $tag = null)
     {
-        if (null === $filterName) {
-            $filterName = new ConstantExpression('raw', $node->getTemplateLine());
-        }
-        if (null === $arguments) {
-            $arguments = new Node();
-        }
-
-        parent::__construct($node, $filterName, $arguments, $lineno ?: $node->getTemplateLine(), $tag ?: $node->getNodeTag());
+        parent::__construct($node, $filterName ?: new ConstantExpression('raw', $node->getTemplateLine()), $arguments ?: new Node(), $lineno ?: $node->getTemplateLine(), $tag ?: $node->getNodeTag());
     }
 
     public function compile(Compiler $compiler): void
