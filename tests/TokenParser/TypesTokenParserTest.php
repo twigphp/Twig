67dabd8182bccd557070cffce843b3f4226e231b
Jeroen Versteeg <jeroen@alisqi.com>
2024-08-23 16:15:55 +0200

    Add types tag

---
+++ tests/TokenParser/TypesTokenParserTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Twig\Tests\TokenParser;
+
+use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use Twig\Parser;
+use Twig\Source;
+
+class TypesTokenParserTest extends TestCase
+{
+    /** @dataProvider getMappingTests */
+    public function testMappingParsing(string $template, array $expected): void
+    {
+        $env = new Environment(new ArrayLoader(), ['cache' => false, 'autoescape' => false]);
+        $stream = $env->tokenize($source = new Source($template, ''));
+        $parser = new Parser($env);
+
+        $typesNode = $parser->parse($stream)->getNode('body')->getNode('0');
+
+        self::assertEquals($expected, $typesNode->getAttribute('mapping'));
+    }
+
+    public function getMappingTests(): array
+    {
+        return [
+            // empty mapping
+            [
+                '{% types {} %}',
+                [],
+            ],
+
+            // simple
+            [
+                '{% types {foo: "bar"} %}',
+                [
+                    'foo' => ['type' => 'bar', 'optional' => false]
+                ],
+            ],
+
+            // trailing comma
+            [
+                '{% types {foo: "bar",} %}',
+                [
+                    'foo' => ['type' => 'bar', 'optional' => false]
+                ],
+            ],
+
+            // optional name
+            [
+                '{% types {foo?: "bar"} %}',
+                [
+                    'foo' => ['type' => 'bar', 'optional' => true]
+                ],
+            ],
+
+            // multiple pairs, duplicate values
+            [
+                '{% types {foo: "foo", bar?: "foo", baz: "baz"} %}',
+                [
+                    'foo' => ['type' => 'foo', 'optional' => false], 
+                    'bar' => ['type' => 'foo', 'optional' => true], 
+                    'baz' => ['type' => 'baz', 'optional' => false]
+                ],
+            ],
+        ];
+    }
+}
