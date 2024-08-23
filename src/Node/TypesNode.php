307a935eb93aa64b32187c28ef27cd73fa581229
Jeroen Versteeg <jeroen@alisqi.com>
2024-08-23 16:48:52 +0200

    TypesNode validates mapping
    Keys must be NameExpressions, values must be string constants.

--- src/Node/TypesNode.php
+++ src/Node/TypesNode.php
@@ -4,18 +4,44 @@ namespace Twig\Node;
 
 use Twig\Compiler;
 use Twig\Node\Expression\ArrayExpression;
+use Twig\Node\Expression\ConstantExpression;
+use Twig\Node\Expression\NameExpression;
 
 /**
  * Represents a types node.
  *
  * @author Jeroen Versteeg <jeroen@alisqi.com>
- * @see https://github.com/twigphp/Twig/issues/4165
+ * @see    https://github.com/twigphp/Twig/issues/4165
  */
 class TypesNode extends Node implements NodeCaptureInterface
 {
     public function __construct(ArrayExpression $typesNode, int $lineno, ?string $tag = null)
     {
+        $this->validateMapping($typesNode);
+
         parent::__construct(['mapping' => $typesNode], [], $lineno, $tag);
+    }
+
+    protected function validateMapping(ArrayExpression $typesNode): void
+    {
+        foreach ($typesNode->getKeyValuePairs() as $i => $pair) {
+            $keyExpression = $pair['key'];
+            $valueExpression = $pair['value'];
+
+            if (!$keyExpression instanceof NameExpression) {
+                throw new \InvalidArgumentException("Key at index $i is not a NameExpression");
+            }
+            $name = $keyExpression->getAttribute('name');
+
+            if (!$valueExpression instanceof ConstantExpression) {
+                throw new \InvalidArgumentException("Value for key \"$name\" is not a ConstantExpression");
+            }
+            $value = $valueExpression->getAttribute('value');
+
+            if (!is_string($value)) {
+                throw new \InvalidArgumentException("Value for key \"$name\" is not a string");
+            }
+        }
     }
 
     public function compile(Compiler $compiler)
--- tests/Node/TypesTest.php
+++ tests/Node/TypesTest.php
@@ -11,7 +11,7 @@ use Twig\Test\NodeTestCase;
 class TypesTest extends NodeTestCase
 {
     /** @return ArrayExpression */
-    private function createArrayExpression()
+    private function getValidMapping()
     {
         // {foo: 'string', bar: 'int'}
         return new ArrayExpression([
@@ -25,10 +25,79 @@ class TypesTest extends NodeTestCase
 
     public function testConstructor()
     {
-        $types = $this->createArrayExpression();
+        $types = $this->getValidMapping();
         $node = new TypesNode($types, 1);
 
         $this->assertEquals($types, $node->getNode('mapping'));
+    }
+
+    /** @return array<array<ArrayExpression>> */
+    public function getInvalidMappings()
+    {
+        return [
+            // {'foo': string}
+            [
+                new ArrayExpression([
+                    new ConstantExpression('foo', 1),
+                    new ConstantExpression('string', 1),
+                ], 1),
+                'Key at index 0 is not a NameExpression'
+            ],
+
+            // [13, 37]
+            [
+                new ArrayExpression([
+                    new ConstantExpression(13, 1),
+                    new ConstantExpression(37, 1),
+                ], 1),
+                'Key at index 0 is not a NameExpression'
+            ],
+
+            // {foo: bar}
+            [
+                new ArrayExpression([
+                    new NameExpression('foo', 1),
+                    new NameExpression('bar', 1),
+                ], 1),
+                'Value for key "foo" is not a ConstantExpression'
+            ],
+
+            // {foo: true}
+            [
+                new ArrayExpression([
+                    new NameExpression('foo', 1),
+                    new ConstantExpression(true, 1),
+                ], 1),
+                'Value for key "foo" is not a string'
+            ],
+
+            // {foo: 123}
+            [
+                new ArrayExpression([
+                    new NameExpression('foo', 1),
+                    new ConstantExpression(123, 1),
+                ], 1),
+                'Value for key "foo" is not a string'
+            ],
+
+            // {foo: {}}}
+            [
+                new ArrayExpression([
+                    new NameExpression('foo', 1),
+                    new ConstantExpression(new ArrayExpression([], 1), 1),
+                ], 1),
+                'Value for key "foo" is not a string'
+            ],
+        ];
+    }
+
+    /** @dataProvider getInvalidMappings */
+    public function testConstructorThrowsOnInvalidMapping(ArrayExpression $mapping, string $message)
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage($message);
+
+        new TypesNode($mapping, 1);
     }
 
     public function getTests()
@@ -36,7 +105,7 @@ class TypesTest extends NodeTestCase
         return [
             // 1st test: Node shouldn't compile at all
             [
-                new TypesNode($this->createArrayExpression(), 1),
+                new TypesNode($this->getValidMapping(), 1),
                 ''
             ]
         ];
