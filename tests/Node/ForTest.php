47d745b7edaf1e6f38c799418c7bcb0ad536677a
Fabien Potencier <fabien@potencier.org>
2024-08-28 21:26:55 +0200

    Add back the if condition on for loops

--- tests/Node/ForTest.php
+++ tests/Node/ForTest.php
@@ -14,8 +14,10 @@ namespace Twig\Tests\Node;
 use Twig\Environment;
 use Twig\Loader\ArrayLoader;
 use Twig\Node\Expression\AssignNameExpression;
+use Twig\Node\Expression\ConstantExpression;
 use Twig\Node\Expression\NameExpression;
 use Twig\Node\ForNode;
+use Twig\Node\IfNode;
 use Twig\Node\Node;
 use Twig\Node\PrintNode;
 use Twig\Test\NodeTestCase;
@@ -27,15 +29,18 @@ class ForTest extends NodeTestCase
         $keyTarget = new AssignNameExpression('key', 1);
         $valueTarget = new AssignNameExpression('item', 1);
         $seq = new NameExpression('items', 1);
+        $ifexpr = new ConstantExpression(true, 1);
         $body = new Node([new PrintNode(new NameExpression('foo', 1), 1)], [], 1);
         $else = null;
-        $node = new ForNode($keyTarget, $valueTarget, $seq, null, $body, $else, 1);
+        $node = new ForNode($keyTarget, $valueTarget, $seq, $ifexpr, $body, $else, 1);
         $node->setAttribute('with_loop', false);
 
         $this->assertEquals($keyTarget, $node->getNode('key_target'));
         $this->assertEquals($valueTarget, $node->getNode('value_target'));
         $this->assertEquals($seq, $node->getNode('seq'));
-        $this->assertEquals($body, $node->getNode('body'));
+        $this->assertInstanceOf(IfNode::class, $node->getNode('body'));
+        $this->assertEquals($ifexpr, $node->getNode('body')->getNode('tests')->getNode(0));
+        $this->assertEquals($body, $node->getNode('body')->getNode('tests')->getNode(1));
         $this->assertFalse($node->hasNode('else'));
 
         $else = new PrintNode(new NameExpression('foo', 1), 1);
@@ -131,9 +136,10 @@ EOF
         $keyTarget = new AssignNameExpression('k', 1);
         $valueTarget = new AssignNameExpression('v', 1);
         $seq = new NameExpression('values', 1);
+        $ifexpr = new ConstantExpression(true, 1);
         $body = new Node([new PrintNode(new NameExpression('foo', 1), 1)], [], 1);
         $else = new PrintNode(new NameExpression('foo', 1), 1);
-        $node = new ForNode($keyTarget, $valueTarget, $seq, null, $body, $else, 1);
+        $node = new ForNode($keyTarget, $valueTarget, $seq, $ifexpr, $body, $else, 1);
         $node->setAttribute('with_loop', true);
 
         $tests[] = [$node, <<<EOF
@@ -144,7 +150,9 @@ EOF
     \$__internal_compile_2 = \$context;
     \$context['loop'] = new \Twig\Runtime\LoopContext(\$__internal_compile_0, \$__internal_compile_2, \$blocks, \$__internal_compile_1, \$depth);
     foreach (\$__internal_compile_0 as \$context["k"] => \$context["v"]) {
-        yield {$this->getVariableGetter('foo')};
+        if (true) {
+            yield {$this->getVariableGetter('foo')};
+        }
     }
     if (0 === \$__internal_compile_0->getIndex0()) {
         yield {$this->getVariableGetter('foo')};
