19185947ec75d433a3ac650af32fc05649b95ee1
Fabien Potencier <fabien@potencier.org>
2024-09-09 19:53:56 +0200

    Prepare the 2.16.1 release

--- src/Environment.php
+++ src/Environment.php
@@ -38,11 +38,11 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '2.16.0';
-    public const VERSION_ID = 21600;
+    public const VERSION = '2.16.1';
+    public const VERSION_ID = 21601;
     public const MAJOR_VERSION = 2;
     public const MINOR_VERSION = 16;
-    public const RELEASE_VERSION = 0;
+    public const RELEASE_VERSION = 1;
     public const EXTRA_VERSION = '';
 
     private $charset;
