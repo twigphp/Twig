66658a3c1d0ef09d70508bb2a953f9f6c6c46d8b
Fabien Potencier <fabien@potencier.org>
2024-10-14 15:46:31 +0200

    Check reserved names in TempNameExpression

--- src/Node/Expression/TempNameExpression.php
+++ src/Node/Expression/TempNameExpression.php
@@ -12,6 +12,7 @@
 namespace Twig\Node\Expression;
 
 use Twig\Compiler;
+use Twig\Error\SyntaxError;
 
 class TempNameExpression extends AbstractExpression
 {
@@ -19,6 +20,11 @@ class TempNameExpression extends AbstractExpression
 
     public function __construct(string|int $name, int $lineno)
     {
+        // All names supported by ExpressionParser::parsePrimaryExpression() should be excluded
+        if (\in_array(strtolower($name), ['true', 'false', 'none', 'null'])) {
+            throw new SyntaxError(\sprintf('You cannot assign a value to "%s".', $name), $lineno);
+        }
+
         if (is_int($name) || ctype_digit($name)) {
             $name = (int) $name;
         } elseif (in_array($name, self::RESERVED_NAMES)) {
