55cb67f87ded61d5e33e6b3405ab3ac5d6c5910b
Christian Flothmann <christian.flothmann@open.de>
2026-01-16 09:58:49 +0100

    bump version

--- src/Environment.php
+++ src/Environment.php
@@ -43,11 +43,11 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '3.22.3-DEV';
-    public const VERSION_ID = 32203;
+    public const VERSION = '3.23.0-DEV';
+    public const VERSION_ID = 32300;
     public const MAJOR_VERSION = 3;
-    public const MINOR_VERSION = 22;
-    public const RELEASE_VERSION = 3;
+    public const MINOR_VERSION = 23;
+    public const RELEASE_VERSION = 0;
     public const EXTRA_VERSION = 'DEV';
 
     private $charset;
