84982072c79a7417b0d158a401d91344f3658299
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-05-15 15:14:02 +0200

    Fix XSS by adjusting `is_safe` annotation on HTML-emitting filters
    The `html_to_markdown` filter emits plain Markdown text, so the
    `is_safe` annotation is dropped entirely and autoescape now handles
    its output according to the surrounding context.
    
    The `markdown_to_html` and `inline_css` filters emit HTML, not text
    safe in every escaping context, so `is_safe => ['all']` produced
    unescaped HTML when their output was interpolated into a JS, CSS or
    URL context. The annotation is now `is_safe => ['html']`.

---
+++ extra/cssinliner-extra/Tests/FunctionalTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Twig.
+ *
+ * (c) Fabien Potencier
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Twig\Extra\CssInliner\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Twig\Environment;
+use Twig\Extra\CssInliner\CssInlinerExtension;
+use Twig\Loader\ArrayLoader;
+
+class FunctionalTest extends TestCase
+{
+    public function testInlineCssIsNotSafeInJsContext()
+    {
+        $twig = new Environment(new ArrayLoader([
+            'index' => "{% autoescape 'js' %}{% apply inline_css %}<p>x</p>{% endapply %}{% endautoescape %}",
+        ]));
+        $twig->addExtension(new CssInlinerExtension());
+
+        $output = $twig->render('index');
+
+        $this->assertStringNotContainsString('<p>', $output);
+        $this->assertStringNotContainsString('</p>', $output);
+        $this->assertMatchesRegularExpression('{\\\\u003[Cc]p\\\\u003[Ee]x\\\\u003[Cc]\\\\/p\\\\u003[Ee]}', $output);
+    }
+}
