597c12ed286fb9d1701a36684ce6e0cbe28ebc8b
Fabien Potencier <fabien@potencier.org>
2026-07-03 22:44:34 +0200

    Prepare the 3.28.0 release

--- src/Environment.php
+++ src/Environment.php
@@ -43,12 +43,12 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '3.28.0-DEV';
+    public const VERSION = '3.28.0';
     public const VERSION_ID = 32800;
     public const MAJOR_VERSION = 3;
     public const MINOR_VERSION = 28;
     public const RELEASE_VERSION = 0;
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     private $charset;
     private $loader;
